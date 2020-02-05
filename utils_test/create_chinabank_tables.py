da76b1a7ef3de0b4473fcf2fbd45962df1a4cb24
furuiyang <furuiyang0715@gmail.com>
2020-02-05 14:19:28 +0800

    新闻发布会建表

--- utils_test/create_chinabank_tables.py
+++ utils_test/create_chinabank_tables.py
@@ -49,6 +49,7 @@ def create_table_gov_stats():
     创建国家统计局的数据表
     :return:
     """
+    # 国家统计局 - 最新发布
     sql = """
     DROP TABLE IF EXISTS `gov_stats_zxfb`;
 
@@ -68,3 +69,24 @@ def create_table_gov_stats():
 
     show full columns from gov_stats_zxfb;
     """
+
+    # 国家统计局 - 新闻发布会
+    sql = """
+        DROP TABLE IF EXISTS `gov_stats_xwfbh`;
+
+        CREATE TABLE `gov_stats_xwfbh` (
+          `id` int(11) NOT NULL AUTO_INCREMENT,
+          `pub_date` datetime not null  COMMENT '发布时间',
+          `title` varchar(64) collate utf8_bin DEFAULT NULL COMMENT '文章标题',
+          `link` varchar(128) collate utf8_bin DEFAULT NULL COMMENT '文章详情页链接',
+          `article` text collate utf8_bin DEFAULT NULL COMMENT '详情页内容',
+          `CREATETIMEJZ` datetime DEFAULT CURRENT_TIMESTAMP,
+          `UPDATETIMEJZ` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+          PRIMARY KEY (`id`), 
+          KEY `pub_date` (`pub_date`)
+        ) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COMMENT='国家统计局[新闻发布会]'; 
+
+        ALTER TABLE `gov_stats_xwfbh` ADD unique(`link`); 
+
+        show full columns from gov_stats_xwfbh;
+        """
