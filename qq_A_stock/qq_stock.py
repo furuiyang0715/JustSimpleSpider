82a8391556ff468a37389034735e647e9c5a7a25
furuiyang <furuiyang0715@gmail.com>
2020-02-10 14:00:18 +0800

    实现文章页的解析

--- qq_A_stock/qq_stock.py
+++ qq_A_stock/qq_stock.py
@@ -1,7 +1,12 @@
+import json
+import pprint
+import sys
+import time
 from queue import Queue
 
 import requests
 from fake_useragent import UserAgent
+from gne import GeneralNewsExtractor
 
 ua = UserAgent()
 
@@ -14,6 +19,7 @@ class qqStock(object):
        "&ext=3911,3922,3923,3914,3913,3930,3915,3918,3908&callback=__jp1".format(self.token)
         self.q = Queue()
         self.proxy = None
+        self.extractor = GeneralNewsExtractor()
 
     def update_proxies(self):
         # run()
@@ -44,12 +50,80 @@ class qqStock(object):
 
     def _get(self, url):
             proxy = self._get_proxy()
+            print("获取到的代理是{}".format(proxy))
             ret = requests.get(url, headers={"User-Agent": ua.random}, proxies={"http": proxy})
             return ret
 
+    def _parse_article(self, item):
+        vurl = item.get("link")
+        body = self._get(vurl).text
+        result = self.extractor.extract(body)
+        # print(pprint.pformat(result))
+        item['article'] = result.get("content")
+        item['pub_date'] = result.get("publish_time")
+        item['title'] = result.get("title")
+
+        return item
+
+    def _get_list(self):
+        while True:
+            try:
+                list_resp = self._get(self.list_url)
+            except:
+                self.proxy = None
+                time.sleep(3)
+            else:
+                break
+
+        if list_resp.status_code == 200:
+            print("请求列表页成功 ")
+            body = list_resp.text
+            body = body.lstrip("__jp1(")
+            body = body.rstrip(")")
+            body = json.loads(body)
+            # print(body)
+            datas = body.get("data")  # list 数据列表
+            # 有两种类型的文章 一种是直接的文章列表页 一种是专题
+            specials = []
+            articles = []
+            for data in datas:
+                if data.get("article_type") == 120:
+                    specials.append(data)
+                elif data.get("article_type") == 0:
+                    articles.append(data)
+                else:
+                    raise Exception("请检查数据")
+            for article in articles:
+                yield article
+
+            # for special in specials:
+            #     # 获取到专题的详情页
+            #     special_detail = special.get("vurl")
+            #     # print(special_detail)
+
+            # print(specials)
+            # print(articles)
+
+
+            pass
+        pass
+
+    def items_gener(self):
+        article_gener = self._get_list()
+        for article in article_gener:
+            # 检查详情页面是否爬取过
+
+            # 创建一个数据对象
+            item = {}
+            vurl = article.get("vurl")
+            item['link'] = vurl
+            item = self._parse_article(item)
+            yield item
+
     def start(self):
-        ret = self._get(self.list_url)
-        return ret
+        for item in self.items_gener():
+            # 保存到数据库
+            print("{} 已入库".format(item['title']))
 
 
 if __name__ == "__main__":
@@ -57,5 +131,4 @@ if __name__ == "__main__":
     # proxy = d._get_proxy()
     # print(proxy)
 
-    ret = d.start()
-    print(ret)
+    d.start()
