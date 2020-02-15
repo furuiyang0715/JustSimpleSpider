7f8443f21939538324a670556a0b78ba767241ec
furuiyang <furuiyang0715@gmail.com>
2020-02-15 14:52:52 +0800

    更改爬取策略

--- EastMoneyCarticle/carticle.py
+++ EastMoneyCarticle/carticle.py
@@ -2,26 +2,36 @@ import json
 import random
 import re
 import string
+import sys
 import time
 import traceback
-from urllib.parse import urlencode
-
 import pymysql
-from gne import GeneralNewsExtractor
 import requests
-from lxml import html
 
-from requests.exceptions import ProxyError,Timeout,ConnectionError,ChunkedEncodingError
+from lxml import html
+from urllib.parse import urlencode
+from gne import GeneralNewsExtractor
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from requests.exceptions import ProxyError, Timeout, ConnectionError, ChunkedEncodingError
+
 from EastMoneyCarticle.configs import MYSQL_HOST, MYSQL_PORT, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB, MYSQL_TABLE
 from EastMoneyCarticle.sql_client import PyMysqlBase
 
+import logging
+
+from EastMoneyCarticle.sql_pool import PyMysqlPoolBase
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
 
 class CArticle(object):
-    def __init__(self):
+    def __init__(self, key):
+        self.key = key
         self.start_url = 'http://api.so.eastmoney.com/bussiness/Web/GetSearchList?'
         self.page_size = 10
         self.headers = {
-            "Referer": "http://so.eastmoney.com/CArticle/s?keyword=%E6%A0%BC%E5%8A%9B%E7%94%B5%E5%99%A8",
+            "Referer": "http://so.eastmoney.com/CArticle/s?keyword={}".format(self.key.encode()),
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36",
 
 
@@ -36,9 +46,11 @@ class CArticle(object):
             "password": MYSQL_PASSWORD,
             "db": MYSQL_DB,
         }
-        self.sql_client = PyMysqlBase(**conf)
+        # self.sql_client = PyMysqlBase(**conf)
+        self.sql_pool = PyMysqlPoolBase(**conf)
         self.proxy = self.get_proxy()
         self.error_detail = []
+        self.error_list = []
 
     def get_proxy(self):
         r = requests.get('http://localhost:8888/get')
@@ -52,20 +64,21 @@ class CArticle(object):
 
     def get(self, url):
         count = 0
-        proxy = self.proxy
-        print(f"当前代理 {proxy}")
         while True:
             count = count + 1
             try:
-                resp = self.crawl(url, proxy)
+                resp = self.crawl(url, self.proxy)
                 if resp.status_code == 200:
                     return resp
-                elif count >= 3:
-                    print('抓取网页失败')
+                elif count >= 5:
+                    logger.warning(f'抓取网页{url}最终失败')
                     break
+                else:
+                    self.proxy = self.get_proxy()
+                    logger.info(f"无效状态码{resp.status_code}, 更换代理{self.proxy}\n")
             except (ChunkedEncodingError, ConnectionError, Timeout, UnboundLocalError, UnicodeError, ProxyError):
-                proxy = self.get_proxy()
-                print('代理失败,更换代理', '\n')
+                self.proxy = self.get_proxy()
+                logger.info(f'代理失败,更换代理{self.proxy} \n')
 
     def make_query_params(self, msg, page):
         """
@@ -89,11 +102,15 @@ class CArticle(object):
         resp = self.get(list_url)
         if resp:
             return resp.text
+        else:
+            self.error_list.append(list_url)
 
     def get_detail(self, detail_url):
         resp = self.get(detail_url)
         if resp:
             return resp.text
+        else:
+            self.error_detail.append(detail_url)
 
     def parse_list(self, list_page):
         try:
@@ -102,8 +119,10 @@ class CArticle(object):
         except:
             return None
         else:
-            for data in list_data:
-                yield data
+            if list_data:
+                return list_data
+            else:
+                return []
 
     def contract_sql(self, to_insert):
         """
@@ -123,13 +142,17 @@ class CArticle(object):
     def save(self, to_insert):
         try:
             insert_sql, values = self.contract_sql(to_insert)
-            count = self.sql_client.insert(insert_sql, values)
+            # count = self.sql_client.insert(insert_sql, values)
+            count = self.sql_pool.insert(insert_sql, values)
         except pymysql.err.IntegrityError:
-            print("重复 ")
-            return 1
+            logger.warning("重复 ")
+            self.sql_pool.connection.rollback()
         except:
-            print("失败")
+            logger.warning("失败")
             traceback.print_exc()
+            self.sql_pool.connection.rollback()
+            # 插入失败之后需要进行回滚
+
         else:
             return count
 
@@ -148,44 +171,68 @@ class CArticle(object):
         contents = "\r\n".join(contents)
         return contents
 
-    def process_item(self, item):
-        return item
+    # def process_item(self, item):
+    #     return item
 
-    def _run_page(self, key, page):
-        list_url = self.start_url + urlencode(self.make_query_params(key, page))
+    def _exist(self, link):
+        ret = self.sql_pool.select_one(f"select * from {self.table} where link = '{link}';")
+        if ret:
+            return True
+        else:
+            return False
+
+    def __del__(self):
+        print("数据库提交 释放资源")
+        self.sql_pool.dispose()
+
+    def _run_page(self, page):
+        list_url = self.start_url + urlencode(self.make_query_params(self.key, page))
         list_page = self.get_list(list_url)
-
         if list_page:
-            list_gener = self.parse_list(list_page)
+            list_gener = self.parse_list(list_page)  # list
+            if not list_gener:
+                logger.info(f"{self.key} 爬取完毕 ")
+                return
+
             for data in list_gener:
                 item = dict()
-                item['code'] = key
+                item['code'] = self.key
+                link = data.get("ArticleUrl")
+                if self._exist(link):
+                    logger.info("pass")
+                    continue
                 item['link'] = data.get("ArticleUrl")
                 item['title'] = data.get("Title")
                 item['pub_date'] = data.get("ShowTime")
-                detail_body = self.get_detail(data.get("ArticleUrl"))
-                article = None
-                if detail_body:
-                    article = self.parse_detail(detail_body)
-                if article:
-                    item['article'] = article
-                    # 将 item 存储到数据库中
-                    to_insert = self.process_item(item)
-                    # print(to_insert)
-                    ret = self.save(to_insert)
-                    if not ret:
-                        # 记录失败的详情页链接
-                        self.error_detail.append(data.get("ArticleUrl"))
+                print("item", item)
+                ret = self.save(item)
+                if not ret:
+                    logger.warning(f"插入失败 {item}")
+        return page
 
-    def _start(self, key):
-        for page in range(1, 5 * 10 ** 4):
-            self._run_page(key, page)
+
+class Schedule(object):
+    def __init__(self):
+        self.keys = ['格力电器', '视源股份', '科大讯飞']
+
+    def insert_list_info(self, key):
+        c = CArticle(key)
+        now = lambda: time.time()
+        t1 = now()
+        cur = t1
+        for page in range(1, 10000):
+            page = c._run_page(page)
+            if not page:
+                break
+            print(f"第 {page} 页, 累计用时 {now() - t1}, 当前页用用时 {now() - cur} ")
+            cur = now()
+        print(c.error_list)
+        print(c.error_detail)
+
+    def run(self):
+        self.insert_list_info("科大讯飞")
 
 
 if __name__ == "__main__":
-    c = CArticle()
-    # print(c.get_proxy())
-    # ret = c.get("https://www.taoguba.com.cn/quotes/sz000651")
-    # print(ret)
-    key = "格力电器"
-    c._start(key)
+    schedule = Schedule()
+    schedule.run()
