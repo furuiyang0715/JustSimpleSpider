44ef4d33eb160e95709b792e5edf6ce603289bb1
furuiyang <furuiyang0715@gmail.com>
2020-02-17 17:26:10 +0800

    公告快讯

--- cnstock/hongguan.py
+++ cnstock/hongguan.py
@@ -153,9 +153,13 @@ class CNStock(object):
                     count = 0
 
 
-# runner = CNStock(topic='qmt-sns_yw')   #  宏观
-# runner = CNStock(topic='qmt-sns_jg')   # 金融
-runner = CNStock(topic="qmt-scp_gsxw")   # 公司聚焦
+if __name__ == "__main__":
+    # runner = CNStock(topic='qmt-sns_yw')   #  宏观
+    # runner = CNStock(topic='qmt-sns_jg')   # 金融
+    # runner = CNStock(topic="qmt-scp_gsxw")   # 公司聚焦
+    runner = CNStock(topic="qmt-tjd_ggkx")   # 公告快讯
 
-
-runner.start()
+    # 测试解析详情页可以实现自动翻页 ...
+    # ret = runner.get_detail("http://ggjd.cnstock.com/company/scp_ggjd/tjd_ggjj/202002/4489878.htm")
+    # print(ret)
+    runner.start()
