1117b35fb956551252960f225ab4bf573b1acd09
furuiyang <furuiyang0715@gmail.com>
2020-02-21 20:39:49 +0800

    xingu module .

--- PublicOpinion/stcn_yaowen.py
+++ PublicOpinion/stcn_yaowen.py
@@ -476,40 +476,16 @@ class STCN_DaPan(STCN_Kuaixun):
         return items
 
 
-class STCN_BanKuai(STCN_Kuaixun):
+class STCN_BanKuai(STCN_DaPan):
     def __init__(self):
         super(STCN_BanKuai, self).__init__()
         self.format_url = "http://stock.stcn.com/bankuai/{}.shtml"
 
-    def _parse_list_body(self, body):
-        # print(body)
-        # sys.exit(0)
-        doc = html.fromstring(body)
-        items = []
-        # 列表文章
-        columns = doc.xpath("//ul[@id='news_list2']/li")
-        num = 0
-        for column in columns:
-            num += 1
-            # print(column.tag)
-            title = column.xpath("./a/@title")[0]
-            link = column.xpath("./a/@href")[0]
-            pub_date = column.xpath("./span")[0].text_content()
-            pub_date = '{} {}'.format(pub_date[:10], pub_date[10:])
-            item = dict()
-            item['title'] = title
-            item['link'] = link
-            item['pub_date'] = pub_date
-            detail_body = self._get(link)
-            if detail_body:
-                article = self._parse_detail(detail_body)
-                if article:
-                    item['article'] = self._process_content(article)
-                    print(item)
-                    items.append(item)
-        # print(num)
-        # sys.exit(0)
-        return items
+
+class STCN_XinGu(STCN_DaPan):
+    def __init__(self):
+        super(STCN_XinGu, self).__init__()
+        self.format_url = "http://stock.stcn.com/xingu/{}.shtml"
 
 
 if __name__ == "__main__":
@@ -525,9 +501,11 @@ if __name__ == "__main__":
 
     # d = STCN_Market()   # 股市
 
-    # d = STCN_DaPan()   # 股市
+    # d = STCN_DaPan()   # 大盘
 
-    d = STCN_BanKuai()   # 版块
+    # d = STCN_BanKuai()   # 版块
+
+    d = STCN_XinGu()   # 新股
 
     d._start()
 
