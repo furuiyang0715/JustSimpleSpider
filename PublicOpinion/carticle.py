adcff0e6f8866912fe95ab452c3c35cdce07c6d9
furuiyang <furuiyang0715@gmail.com>
2020-02-25 10:29:10 +0800

    东财增量

--- PublicOpinion/carticle.py
+++ PublicOpinion/carticle.py
@@ -1,9 +1,12 @@
-# -*- coding: utf-8 -*-
+import datetime
 import json
 import logging
+import os
+import pprint
 import random
 import re
 import string
+import sys
 import time
 import traceback
 from urllib.parse import urlencode
@@ -11,26 +14,24 @@ from urllib.parse import urlencode
 import pymysql
 import requests
 from lxml import html
-import sys
 
-sys.path.append("./../")
-from PublicOpinion.configs import MYSQL_DB, MYSQL_HOST, MYSQL_PORT, MYSQL_USER, MYSQL_PASSWORD, DC_HOST, DC_PORT, \
-    DC_USER, DC_PASSWD, DC_DB, LOCAL, LOCAL_MYSQL_HOST, LOCAL_MYSQL_PORT, LOCAL_MYSQL_USER, LOCAL_MYSQL_PASSWORD, \
-    LOCAL_MYSQL_DB
+from PublicOpinion.configs import LOCAL, MYSQL_DB, LOCAL_MYSQL_HOST, LOCAL_MYSQL_PORT, LOCAL_MYSQL_USER, \
+    LOCAL_MYSQL_PASSWORD, LOCAL_MYSQL_DB, MYSQL_HOST, MYSQL_PORT, MYSQL_USER, MYSQL_PASSWORD
 from PublicOpinion.sql_pool import PyMysqlPoolBase
 
 
+PROXY_URL = os.environ.get("PROXY_URL", "http://172.17.0.4:8888/get")   # 远程的代理地址
+LOCAL_PROXY_URL = os.environ.get("LOCAL_PROXY_URL", "http://127.0.0.1:8888/get")   # 本地的代理url
 logger = logging.getLogger()
 
 
-class CArticleLoder(object):
+class CArticle(object):
     def __init__(self, key):
         # 本地运行亦或者是在服务器上运行
         self.local = LOCAL
-        # 是否使用阿布云代理
-        self.abu = False
         # 股票代码中文简称
         self.key = key
+        # 请求的起始 url
         self.start_url = 'http://api.so.eastmoney.com/bussiness/Web/GetSearchList?'
         self.page_size = 10
         self.headers = {
@@ -58,12 +59,18 @@ class CArticleLoder(object):
                 "db": MYSQL_DB,
             }
         self.sql_pool = PyMysqlPoolBase(**conf)
-        # 不使用阿布云的情况下 初始化代理
-        if not self.abu:
-            self.proxy = self._get_proxy()
+
         # 记录出错的列表页 以及 详情页 url
         self.error_detail = []
         self.error_list = []
+
+        # 初始化代理
+        self.proxy = self._get_proxy()
+        self.dt_format = '%Y-%m-%d %H:%M:%S'
+        # 增量爬取的临界时间
+        self.limit_time = datetime.datetime(2020, 2, 14)
+        # 是否使用代理
+        self.use_proxy = 1
 
     def make_query_params(self, msg, page):
         query_params = {
@@ -103,51 +110,11 @@ class CArticleLoder(object):
         else:
             return count
 
-    def _abu_get(self, url):
-        """使用阿布云代理 默认失败后重新发起请求"""
-        proxy_host = "http-cla.abuyun.com"
-        proxy_port = 9030
-        # 代理隧道验证信息
-        proxy_user = "H74JU520TZ0I2SFC"
-        proxy_pass = "7F5B56602A1E53B2"
-        proxy_meta = "http://%(user)s:%(pass)s@%(host)s:%(port)s" % {
-            "host": proxy_host,
-            "port": proxy_port,
-            "user": proxy_user,
-            "pass": proxy_pass,
-        }
-        proxies = {
-            "http": proxy_meta,
-            "https": proxy_meta,
-        }
-        retry = 2  # 重试三次 事不过三^_^
-        while True:
-            try:
-                resp = requests.get(url,
-                                    proxies=proxies,
-                                    headers=self.headers,
-                                    timeout=3,
-                                    )
-                if resp.status_code == 200:
-                    return resp
-                else:
-                    print(resp.status_code, "retry")
-                    retry -= 1
-                    if retry <= 0:
-                        return None
-                    time.sleep(3)
-            except:
-                print("error retry")
-                retry -= 1
-                if retry <= 0:
-                    return None
-                time.sleep(3)
-
     def _get_proxy(self):
         if self.local:
-            r = requests.get('http://192.168.0.102:8888/get')
+            r = requests.get(LOCAL_PROXY_URL)
         else:
-            r = requests.get('http://172.17.0.4:8888/get')
+            r = requests.get(PROXY_URL)
         proxy = r.text
         return proxy
 
@@ -164,28 +131,32 @@ class CArticleLoder(object):
         return r
 
     def _get(self, url):
-        if self.abu:
-            return self._abu_get(url)
-
-        count = 0
-        while True:
-            count = count + 1
-            try:
-                resp = self._crawl(url, self.proxy)
-                if resp.status_code == 200:
-                    return resp
-                elif resp.status_code == 404:
-                    self._delete_detail_404(url)
-                    return None
-                elif count > 2:
-                    logger.warning(f'抓取网页{url}最终失败')
-                    break
-                else:
+        if self.use_proxy:
+            count = 0
+            while True:
+                count = count + 1
+                try:
+                    resp = self._crawl(url, self.proxy)
+                    if resp.status_code == 200:
+                        return resp
+                    elif resp.status_code == 404:
+                        self._delete_detail_404(url)
+                        return None
+                    elif count > 2:
+                        logger.warning(f'抓取网页{url}最终失败')
+                        break
+                    else:
+                        self.proxy = self._get_proxy()
+                        logger.warning(f"无效状态码{resp.status_code}, 更换代理{self.proxy}\n")
+                except:
                     self.proxy = self._get_proxy()
-                    logger.warning(f"无效状态码{resp.status_code}, 更换代理{self.proxy}\n")
+                    logger.warning(f'代理失败,更换代理{self.proxy} \n')
+        else:
+            try:
+                resp = requests.get(url)
             except:
-                self.proxy = self._get_proxy()
-                logger.warning(f'代理失败,更换代理{self.proxy} \n')
+                return
+            return resp
 
     def _parse_detail(self, detail_page):
         doc = html.fromstring(detail_page)
@@ -201,18 +172,6 @@ class CArticleLoder(object):
                 contents.append(p_node.text_content())
         contents = "\r\n".join(contents)
         return contents
-
-    def _select_key_links(self):
-        select_all_sql = f"select link from {self.table} where code = '{self.key}' and article is NULL;"
-        # links = self.sql_pool.select_many(select_all_sql, size=10)
-        links = self.sql_pool.select_all(select_all_sql)
-        return links
-
-    def _select_rest_all_links(self):
-        select_all_sql = f"select link from {self.table} where article is NULL;"
-        links = self.sql_pool.select_many(select_all_sql, size=20)
-        # links = self.sql_pool.select_all(select_all_sql)
-        return links
 
     def transferContent(self, content):
         if content is None:
@@ -260,7 +219,6 @@ class CArticleLoder(object):
 
     def _update_detail(self, link, article):
         # 直接插入文本内容可能出错 需对其进行处理
-        # article = self.transferContent(article)
         article = self._process_content(article)
         print("文章内容是: \n", article)
         update_sql = f"update {self.table} set article =%s where link =%s;"
@@ -330,121 +288,67 @@ class CArticleLoder(object):
         except:
             pass
 
+    def _start(self):
+        # 本类是针对某一个具体的 code 来进行爬取的
+        # 所以在  _start 之外还会有一个总的 "调度函数"
 
-class Schedule(object):
-    def __init__(self):
-        self.keys = sorted(self.dc_info().values())
+        # (1) 生成 list_url
+        for page in range(1, 1000):
+            print(page)
+            list_url = self.start_url + urlencode(self.make_query_params(self.key, page))
+            # print(list_url)
 
-    def dc_info(self):  # {'300150.XSHE': '世纪瑞尔',
-        """
-        从 datacanter.const_secumain 数据库中获取当天需要爬取的股票信息
-        返回的是 股票代码: 中文名简称 的字典的形式
-        """
-        try:
-            conn = pymysql.connect(host=DC_HOST, port=DC_PORT, user=DC_USER,
-                                   passwd=DC_PASSWD, db=DC_DB)
-        except Exception as e:
-            raise
+            # (2) 获取列表页
+            list_page = self._get_list(list_url)
+            # print(list_page)
 
-        cur = conn.cursor()
-        cur.execute("USE datacenter;")
-        cur.execute("""select SecuCode, ChiNameAbbr from const_secumain where SecuCode \
-            in (select distinct SecuCode from const_secumain);""")
-        dc_info = {r[0]: r[1] for r in cur.fetchall()}
-        cur.close()
-        conn.close()
-        return dc_info
+            # (3) 从列表页解析数据 返回列表
+            list_infos = self._parse_list(list_page)
+            # print(pprint.pformat(list_infos))
 
-    def _start_instance(self, key):
-        c = CArticleLoder(key)
-        now = lambda: time.time()
-        t1 = now()
-        cur = t1
-        for page in range(1, 50000):
-            page = c._save_one_page_list(page)
-            if not page:
-                break
-            print(f"第 {page} 页, 累计用时 {now() - t1}, 当前页用用时 {now() - cur} ")
-            cur = now()
-        c.sql_pool.dispose()
-        with open("record.txt", "a+") as f:
-            f.write(f"{key}: error_list: {c.error_list}, error_detail: {c.error_detail}\r\n")
+            # # 爬取到终点
+            # if not list_infos:
+            #     logger.info(f"{self.key} 爬取完毕 ")
+            #     return
 
-    def run_list(self, start=None, end=None, key=None):
-        if key:
-            self._start_instance(key)
-        else:
-            for key in self.keys[start: end]:
-                self._start_instance(key)
+            if list_infos:
+                # 增量的过程中不再继续爬取
+                show_times = [datetime.datetime.strptime(info.get("ShowTime"), self.dt_format) for info in list_infos]
+                print(show_times)
 
-    def _start_rest_detail(self):
-        c = CArticleLoder("")
-        while True:
-            links = c._select_rest_all_links()
-            if len(links) < 20:
-                break
-            print(links)
-            print("length:", len(links))
-            for link in links:
-                link = link.get("link")
-                detail_resp = c._get(link)
-                print("resp:", detail_resp)
-                if detail_resp:
-                    detail_page = detail_resp.text
-                    article = c._parse_detail(detail_page)
-                    # print("article: ", article)
-                    ret = c._update_detail(link, article)
-                    if ret:
-                        print("更新成功")
-                    else:
-                        print("更新失败")
-            print("一次提交")
-            c.sql_pool.end()
+                if max(show_times) < self.limit_time:
+                    print("增量完毕")
+                    return
 
-    def _start_ins_detail(self, key):
-        c = CArticleLoder(key)
-        links = c._select_key_links()
-        print(links)
-        count = 0
-        for link in links:
-            link = link.get("link")
-            print("当前处理连接:", link)
-            detail_resp = c._get(link)
-            print("响应结果: ", detail_resp)
-            if detail_resp:
-                detail_page = detail_resp.text
-                article = c._parse_detail(detail_page)
-                ret = c._update_detail(link, article)
-                if ret:
-                    print("更新成功")
-                else:
-                    print("更新失败")
-                count += 1
-                if count > 9:
-                    print("提交")
-                    c.sql_pool.end()
-                    count = 0
-        c.sql_pool.dispose()
-
-    def run_detail(self, start=None, end=None, key=None):
-        if key:
-            self._start_ins_detail(key)
-        else:
-            for k in self.keys[start: end]:
-                print(k)
-                self._start_ins_detail(k)
+                # (4) 解析详情页 保存数据
+                for data in list_infos:
+                    item = dict()
+                    item['code'] = self.key
+                    link = data.get("ArticleUrl")
+                    item['link'] = link
+                    item['title'] = data.get("Title")
+                    item['pub_date'] = data.get("ShowTime")
+                    # print("link: ", link)
+                    detail_page = self._get_detail(link)
+                    # print("detail page: ", detail_page)
+                    if detail_page:
+                        article = self._parse_detail(detail_page)
+                        item['article'] = article
+                        print("item", item)
+                        ret = self._save(item)
+                        if not ret:
+                            logger.warning(f"插入失败 {item}")
+                self.sql_pool.end()  # self.sql_pool.connection.commit()
+                print(f"第{page}页保存成功")
+                return page
 
 
 if __name__ == "__main__":
-    s = Schedule()
-    # print(len(s.keys))   # 3923
-    # print(s.keys.index('华邦健康'))
-    # print(s.keys[500])
-    # s.run_list(key="格力电器")
-    # import os
-    # key = os.environ.get("KEY", "格力电器")
-    # start = os.environ.get("START", 0)
-    # end = os.environ.get("END", 0)
-    # print(key, start, end)
-    # s.run_detail(key=key, start=int(start), end=int(end))
-    pass
+    c = CArticle(key='格力电器')
+    # print(c.proxy)
+    c._start()
+
+    # c.use_proxy = 0
+    # detail_page = c._get_detail("http://caifuhao.eastmoney.com/news/20200224214426245917460")
+    # ret = c._parse_detail(detail_page)
+    # print(ret)
