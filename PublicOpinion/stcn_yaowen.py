bffd20a8611aac772b80351fe7ed2297a22387a9
furuiyang <furuiyang0715@gmail.com>
2020-02-25 17:24:39 +0800

    专栏

--- PublicOpinion/stcn_yaowen.py
+++ PublicOpinion/stcn_yaowen.py
@@ -256,62 +256,6 @@ class STCN_ZT(STCN_YaoWen):
                     print(items)
 
 
-class STCN_Column(STCN_YaoWen):
-    def __init__(self):
-        super(STCN_Column, self).__init__()
-        self.list_url = "http://space.stcn.com"
-        self.extractor = GeneralNewsExtractor()
-
-    def _get(self, url):
-        resp = requests.get(url)
-        # print(resp.encoding)
-        if resp.status_code == 200:
-            body = resp.text.encode("ISO-8859-1").decode("utf-8")
-            return body
-
-    def _parse_detail(self, body):
-        try:
-            result = self.extractor.extract(body)
-            content = result.get("content")
-        except:
-            return None
-        else:
-            return content
-
-    def _parse_list_body(self, body):
-        # print(body)
-        doc = html.fromstring(body)
-        items = []
-        # 列表文章
-        columns = doc.xpath('//div[@id="news_list2"]/dl')
-        # print(columns)
-        num = 0
-        for column in columns:
-            num += 1
-            # print(column.tag)
-            title = column.xpath('./dd[@class="mtit"]/a/@title')[0]
-            link = column.xpath('./dd[@class="mtit"]/a/@href')[0]
-
-            pub_date = column.xpath('./dd[@class="mexp"]/span')[0].text_content()
-            yesterday = datetime.datetime.today()-datetime.timedelta(days=1)
-            before_yesterday = datetime.datetime.today()-datetime.timedelta(days=2)
-            pub_date = pub_date.replace("昨天", yesterday.strftime("%Y-%m-%d"))
-            pub_date = pub_date.replace("前天", before_yesterday.strftime("%Y-%m-%d"))
-            # print(title, link, pub_date)
-
-            item = dict()
-            item['title'] = title
-            item['link'] = link
-            item['pub_date'] = pub_date
-            detail_body = requests.get(link).text
-            if detail_body:
-                article = self._parse_detail(detail_body)
-                if article:
-                    item['article'] = self._process_content(article)
-                    print(item)
-                    items.append(item)
-        # print(num)
-        return items
 
 # ==================================================================================================
 
@@ -366,7 +310,6 @@ if __name__ == "__main__":
 
     d = STCN_XWPL()    # 评论
 
-    d = STCN_Column()   # 专栏
 
     d = STCN_ZT()  # 专题  TODO 页面情况太多 暂时不做..
 
