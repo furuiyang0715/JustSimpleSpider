c3392d81b71956df50ba37259833cce6f752ae25
furuiyang <furuiyang0715@gmail.com>
2020-02-25 18:10:05 +0800

    add some .

--- PublicOpinion/stcn/__init__.py
+++ PublicOpinion/stcn/__init__.py
@@ -1,1 +1,68 @@
 ### 证券时报
+
+# 划分依据一
+
+# 要闻: http://news.stcn.com/ [yaowen.py]
+#
+# 快讯 http://kuaixun.stcn.com/index_1.shtml [kuaixun.py]
+#
+# 滚动 http://news.stcn.com/roll/index_1.shtml [roll.py]
+#
+# 深度 http://news.stcn.com/sdbd/1.shtml
+#
+# 评论 http://news.stcn.com/xwpl/1.shtml [xwpl.py]
+#
+# 观察 http://news.stcn.com/sbgc/1.shtml
+#
+# 专栏 http://space.stcn.com/ [space.py]
+#
+# 股市 http://stock.stcn.com/ [market.py]
+#
+# 大盘 http://stock.stcn.com/dapan/1.shtml [dapan.py]
+#
+# 版块 http://stock.stcn.com/bankuai/1.shtml [bankuai.py]
+#
+# 新股 http://stock.stcn.com/xingu/1.shtml [xingu.py]
+#
+# 主力 http://stock.stcn.com/zhuli/1.shtml [zhuli.py]
+#
+# 研报 http://kuaixun.stcn.com/list/kxyb.shtml [yanbao.py]
+#
+# 公司 http://company.stcn.com/ [company.py]
+#
+# 科创版 http://kcb.stcn.com/news/index.shtml [kcb.py]
+#
+# 专题 http://zt.stcn.com/
+#
+# 机构 http://finance.stcn.com/
+#
+# 创投 http://news.stcn.com/xwct/1.shtml
+#
+# 动态 http://company.stcn.com/gsdt/1.shtml
+#
+# 视频
+#
+# 上研院
+#
+# 路演
+#
+# 直播
+#
+# 数据新闻
+#
+# 看点 http://data.stcn.com/kandianshuju/1.shtml
+#
+# 独家数据 http://data.stcn.com/djsj/1.shtml
+#
+# 资金 http://data.stcn.com/zijinliuxiang/1.shtml
+#
+# 科创板信披 http://kcb.stcn.com/
+#
+# 创业板信披 http://chinext.stcn.com/
+#
+# 沪股通 http://hgt.stcn.com/
+#
+# 深股通 http://sgt.stcn.com/
+
+# 划分依据二
+
