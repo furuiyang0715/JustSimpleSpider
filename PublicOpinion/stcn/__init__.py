9b26a78c19fafbe3e8d89f7a24917dccbd3c3fdb
furuiyang <furuiyang0715@gmail.com>
2020-02-26 11:30:20 +0800

    little

--- PublicOpinion/stcn/__init__.py
+++ PublicOpinion/stcn/__init__.py
@@ -6,7 +6,7 @@
 # 要闻-国内 http://news.stcn.com/guonei/1.shtml [guonei.py]
 # 要闻-海外 http://news.stcn.com/xwhw/1.shtml [haiwai.py]
 # 要闻-滚动 http://news.stcn.com/roll/index_1.shtml [roll.py]
-# 要闻-深度 http://news.stcn.com/sdbd/ 【页面空白】
+# 要闻-深度 http://news.stcn.com/sdbd/
 # 要闻-评论 http://news.stcn.com/xwpl/1.shtml [xwpl.py]
 # 要闻-时报观察 http://news.stcn.com/sbgc/ 【空白页面】
 # 要闻-创投 http://news.stcn.com/xwct/1.shtml [chuangtou.py]
@@ -28,104 +28,21 @@
 # 上市公司舆情榜 http://yq.stcn.com/ssgsyqb/ [ssgsyqb.py]
 # 中心动态 http://yq.stcn.com/zxdt/1.shtml [zxdt.py]
 
-# 深度 http://news.stcn.com/sdbd/1.shtml
-#
-# 评论 http://news.stcn.com/xwpl/1.shtml [xwpl.py]
-#
-# 观察 http://news.stcn.com/sbgc/1.shtml 同 要闻-时报观察
 
-#
 # 大盘 http://stock.stcn.com/dapan/1.shtml [dapan.py]
-#
 # 版块 http://stock.stcn.com/bankuai/1.shtml [bankuai.py]
-#
 # 新股 http://stock.stcn.com/xingu/1.shtml [xingu.py]
-#
 # 主力 http://stock.stcn.com/zhuli/1.shtml [zhuli.py]
-#
-# 研报 http://kuaixun.stcn.com/list/kxyb.shtml [yanbao.py]
-#
-# 公司 http://company.stcn.com/ [company.py]
-#
 # 科创版 http://kcb.stcn.com/news/index.shtml [kcb.py]
-#
-# 专题 http://zt.stcn.com/
-#
-# 机构 http://finance.stcn.com/
-#
-# 创投 http://news.stcn.com/xwct/1.shtml
-#
-# 动态 http://company.stcn.com/gsdt/1.shtml
-#
-# 视频
-#
-# 上研院
-#
-# 路演
-#
-# 直播
-#
-# 数据新闻
-#
+# 专题 http://zt.stcn.com/  【暂时不做】
+
+# 公司动态 http://company.stcn.com/gsdt/1.shtml
 # 看点 http://data.stcn.com/kandianshuju/1.shtml
-#
 # 独家数据 http://data.stcn.com/djsj/1.shtml
-#
 # 资金 http://data.stcn.com/zijinliuxiang/1.shtml
-#
 # 科创板信披 http://kcb.stcn.com/
-#
 # 创业板信披 http://chinext.stcn.com/
-#
 # 沪股通 http://hgt.stcn.com/
-#
 # 深股通 http://sgt.stcn.com/
-
-# 划分依据二
-
-###  要闻 http://news.stcn.com/  【重复】
-
-# 要闻-国内 http://news.stcn.com/guonei/1.shtml
-
-# 要闻-海外 http://news.stcn.com/xwhw/1.shtml
-
-# 要闻-滚动  http://news.stcn.com/roll/index_1.shtml 【重复】
-
-# 要闻-深度 http://news.stcn.com/sdbd/1.shtml【重复】
-
-# 要闻-评论 http://news.stcn.com/xwpl/1.shtml 【重复】
-
-# 要闻-时报观察 http://news.stcn.com/sbgc/1.shtml
-
-# 要闻-创投 http://news.stcn.com/xwct/1.shtml
-
-# 要闻-人物 http://news.stcn.com/renwu/1.shtml
-
-# 要闻-时报动态 http://news.stcn.com/sbdt/1.shtml
-
-### 快讯 http://news.stcn.com/sbdt/1.shtml 【重复】
-
-### 专栏 http://space.stcn.com/ 【重复】
-
-### 股市 http://stock.stcn.com/ 【重复】
-
-### 公司 http://company.stcn.com/ 【重复】
-
-### 研报 http://kuaixun.stcn.com/list/kxyb_1.shtml 【重复】
-
-### 机构 http://finance.stcn.com/ 【重复】
-
-### 数据 http://data.stcn.com/djsj/1.shtml
-
-### 舆情 http://yq.stcn.com/
-# 舆情聚焦 http://yq.stcn.com/yqjj/
-
-# 舆情速览  http://yq.stcn.com/yqsl/
-
-# 舆情研究 http://yq.stcn.com/yqyj/
-
-# 上市公司舆情榜 http://yq.stcn.com/ssgsyqb/
-
-# 中心动态 http://yq.stcn.com/zxdt/
 
 
