fcd5d2ad726043d9546a29bfe48edad782230742
furuiyang <furuiyang0715@gmail.com>
2020-02-27 17:44:40 +0800

    早报

---
+++ PublicOpinion/cls_cn/linshi.js
@@ -0,0 +1,109 @@
+
+          __NEXT_DATA__ = {"props":{"isServer":true,
+          "initialState":{"homePage":{"columnList":{},"playing":false,"progress":"0%"},
+          "layout":{"findPwdShow":false,"userIsExplain":false,"logInErrorMsg":"","bindPhoneStatus":"","searchShow":false,
+          "regIsSuccess":false,"imgPreview":{"img":"","visible":false},"logInShow":false,"historyItem":[],
+          "searchValue":"","searchHistory":[],"regErrorMsg":"","siteHost":"www.cls.cn","regShow":false,
+          "regNextPage":"","userMsg":{"accountNumber":"","password":"","userInfo":{},"msgBoxState":false},
+          "hotWord":[],"bindPhoneError":"","navKey":"reference","bindCaptcha":"",
+          "regUser":{"mobleNo":"","captcha":"","bizCode":"","password":"","confirmPassword":"","uname":""}},
+          "common":{"ad":{},"mainRecommend":{"mainRecommendState":true,"mainRecommendList":{}},
+
+          "block":{"blockUpData":{},"blockDownData":{}},"stockHelperState":true,"stock":{"stockUpData":{},"stockDownData":{}},
+          "FAQState":false,"market":{"page":1,"shanghai":{},"shenzhen":{},"middleAndSmall":{},"business":{},"bguIndex":{},"mainIndex":{}},"symbiosis":{},"topData":{}},"telegraph":{"newTelegraphNotificationState":"on","telegraphAudioStatus":true,"newTelegraph":[],"explainTelegraphId":0,"dataList":{},"newNumber":0,"newTelegraphState":true,"getMoreButtonState":"finish","dataSplitTime":0},"theme":{"dataList":{},"dataSplitTime":0,"getMoreButtonState":"finish","page":1,"noMore":false},"depth":{"dataList":{},"dataSplitTime":0,"getMoreButtonState":"finish","page":1,"noMore":false},"reference":{"everydayReceiveList":[],"globaMarketList":[],"hotPlateList":[],"analyzeList":[],"referenceNavNum":1,"queryData":{"type":"1"},"morningNewsList":[{"neican_img":"","calendar_color":"","morningNewsContent":{"is_top":0,"remark":"","schema":"","abstract":"","is_explain":-1,"ad":{"id":0,"title":"","img":"","url":"","monitorUrl":"","video_url":"","adTag":"","fullScreen":0},"share_img":"https://image.cailianpress.com/share/neican.png","img":"",
+          "article_id":448195,"topics":[],"column_id":-1,"level":"C",
+          "author_extends":"sz002241@@歌尔股份##sz300098@@高新兴##sz300024@@机器人##sh603986@@兆易创新##sh600802@@福建水泥##sz000063@@中兴通讯##sh603168@@莎普爱思##sz002279@@久其软件##sz000936@@华西股份##sz300750@@宁德时代##sh600783@@鲁信创投##sz000425@@徐工机械##sz000973@@佛塑科技##sz002480@@新筑股份##sh600276@@恒瑞医药##sh600990@@四创电子##sh600418@@江淮汽车##sz000725@@京东方A##sz002803@@吉宏股份##sh601318@@中国平安##sz002714@@牧原股份",
+          "audio_url":"https://img.cls.cn/images/20200227/tl3VPY95XP.mp3","comment_num":0,"share_num":0,
+          "subjects":[{"article_id":448195,
+          "subject_id":1151,"subject_name":"有声早报","subject_img":"https://image.cailianpress.com/admin/20180720/092659IEVCa6kuvMMP.jpg",
+          "subject_description":"每日7点，最热、最全面的财经资讯尽在财联社早报","category_id":1012,"attention_num":10102,"is_attention":false}],
+          "recommended_reading":[{"img":"https://image.cailianpress.com/admin/20180717/064030xQjSUV9gqbgJ","column_id":"-1",
+          "externalLink":"","author":"","ctime":1531781242,"title":"欧美股市涨跌不一 油价重挫超4%金价创一年新低",
+          "brief":"","readingNum":1134389,"type":"2","id":258961},
+
+          {"img":"https://image.cailianpress.com/admin/20180716/224700sKFZuoPEOELF.jpg","column_id":"-1","externalLink":"","author":"","ctime":1531752467,
+          "title":"【原创】村镇银行发展分化：大行甩卖 农商行则选择扎根扩张","brief":"","readingNum":1255629,"type":"2","id":258902},
+
+          {"img":"https://image.cailianpress.com/admin/20180716/223314chr7Sz4xK85H.jpg","column_id":"-1","externalLink":"","author":"",
+          "ctime":1531751653,"title":"【原创】华大癌变风波或揭示基因检测乱象冰山一角：研发与使用者专业知识严重脱节","brief":"","readingNum":1091521,"type":"2","id":258900}],
+          "bottom_btn":{"btn_icon":"","tips":"","schema":""},"depthRecArticle":[],"visibletagsList":[],"commentary":null,"author":"",
+          "bottom_tips":"财联社声明：文章内容仅供参考，不构成投资建议。投资者据此操作，风险自担。","deny_comment":0,"attachmentsList":[],"imgs":[],
+          "shareurl":"https://api3.cls.cn/share/article/448195?os=web\u0026sv=6.8.0\u0026app=CailianpressWeb","ctime":1582758000,
+          "sort_score":1582758000,"status":1,"reading_num":675354,"column_name":"","short_title":"",
+          "belonging_to":{"id":0,"name":"","image":"","description":"","isAttention":false,"attentionNum":0},
+          "title":"财联社2月27日早报（周四）","is_ad":0,
+          "content":"\u003cp\u003e\u003cstrong\u003e一、【财联社2月27日早间新闻精选】\u003c/strong\u003e\u003c/p\u003e\n\u003cp\u003e\u003cstrong\u003e宏观新闻：\u003c/strong\u003e\u003c/p\u003e\n\u003cp\u003e1、中共中央政治局常务委员会召开会议，习近平在会议上发表重要讲话指出，当前全国疫情防控形势积极向好的态势正在拓展，经济社会发展加快恢复。加强疫情防控这根弦不能松，经济社会发展各项工作要抓紧。\u003c/p\u003e\n\u003cp\u003e2、截至2月25日24时，除湖北以外，全国新增确诊病例5例，新增疑似病例128例，无新增死亡病例，重症病例减少25例。\u003c/p\u003e\n\u003cp\u003e3、中国人民银行26日表示，在前期已经设立3000亿元疫情防控专项再贷款的基础上，增加再贷款再贴现专用额度5000亿元。同时，下调支农、支小再贷款利率0.25个百分点至2.5%。\u003c/p\u003e\n\u003cp\u003e4、周三，创业板指放量跌4.6%，两市近百股跌停，成交额连续六日破万亿，北向资金连续4日净流出。\u003c/p\u003e\n\u003cp\u003e5、财联社报道，昨日市场传闻“科技类ETF被监管窗口指导”，记者从相关基金公司获悉，他们仅接到监管了解情况的问询，并未有限制规模等约束性指导。\u003c/p\u003e\n\u003cp\u003e\u003cstrong\u003e行业新闻：\u003c/strong\u003e\u003c/p\u003e\n\u003cp\u003e1、国务院发布关于开展线上服务进一步加强湖北疫情防控工作的通知。通知主要包括加强远程医疗服务、推进人工智能服务、提升中医诊疗服务等7个方面的内容。\u003c/p\u003e\n\u003cp\u003e2、世界卫生组织(WHO)表示，中国境外日新增新冠肺炎病例已超过中国境内。\u003c/p\u003e\n\u003cp\u003e3、国家邮政局数据显示，1月份，全国快递服务企业业务量完成37.8亿件，同比下降16.4%；业务收入完成500.5亿元，同比下降16.1%。\u003c/p\u003e\n\u003cp\u003e4、长沙市教育局下发通知，各中小学校开学时间需在3月2日后再次延迟，3月9日起直至疫情结束正式复学前，全市各中小学校按照教学计划开展新学期教育教学。\u003c/p\u003e\n\u003cp\u003e5、中央赴湖北指导组指导督导加强武汉以外市州患者救治工作。\u003c/p\u003e\n\u003cp\u003e6、截至北京时间26日18点08分，华为MateXs在华为商城、京东、天猫三大平台的总预约量已经超过36万台。\u003c/p\u003e\n\u003cp\u003e7、商务部介绍，目前20家大型连锁餐饮企业复工率已经达到57%；住宿业的复工率也已经达到50%以上。\u003c/p\u003e\n\u003cp\u003e8、文化和旅游部介绍，对旅游景区开放条件和必要性进行全面地评估，不宜搞“一刀切”，疫情高风险地区旅游景区暂缓开放。\u003c/p\u003e\n\u003cp\u003e9、新闻联播介绍，中国北斗全面助力抗击新冠肺炎疫情，目前，基于全国2600个北斗地基增强系统全天候高稳定性运行，服务可用性高达99.99%，为新冠肺炎防疫工作提供了有力保障。\u003c/p\u003e\n\u003cp\u003e10、国家发展改革委表示，截至2月24日，全国医用N95口罩日产能达到107万只，是2月1日的4.7倍。\u003c/p\u003e\n\u003cp\u003e11、乘联会公布数据，中国2月17-23日当周乘用车市场零售销量日均同比下降83%，日均5411辆。\u003c/p\u003e\n\u003cp\u003e12、韩国中央灾难安全对策本部负责人金刚立表示，政府将疫情预警上调至最高级别的“严重”。\u003c/p\u003e\n\u003cp\u003e13、商务部服贸司司长冼国义表示，预计疫情结束后住宿、餐饮行业会迅速恢复，商务部在研究出台更有针对性的措施。\u003c/p\u003e\n\u003cp\u003e\u003cstrong\u003e公司新闻：\u003c/strong\u003e\u003c/p\u003e\n\u003cp\u003e1、牧原股份发布2019年年度报告，实现归属于母公司所有者的净利润61.14亿元，同比增长近11倍，拟10转7派5.5元。\u003c/p\u003e\n\u003cp\u003e2、宁德时代拟定增募集资金不超200亿元，用于宁德时代湖西锂离子电池扩建项目、江苏时代动力及储能锂离子电池研发与生产项目、补充流动资金。\u003c/p\u003e\n\u003cp\u003e3、新筑股份发布股票交易异常波动公告称，近期市场对 “超级电容器”十分关注，公司的控股子公司奥威科技从事超级电容器业务。\u003c/p\u003e\n\u003cp\u003e4、莎普爱思公告，大股东拟转让股权，公司的控股股东将由陈德康变更为养和投资，公司的实际控制人将由陈德康变更为林弘立、林弘远兄弟。\u003c/p\u003e\n\u003cp\u003e5、江淮汽车公告，经公司自查及向相关方核实，公司与蔚来汽车之间不存在除已披露外的其他合作计划，也没有“混改”的任何计划。\u003c/p\u003e\n\u003cp\u003e6、两连板佛塑科技发布股票交易异常波动公告称，公司计划采购5-6条口罩生产线，但口罩生产不会对公司本年度经营业绩产生重大影响。\u003c/p\u003e\n\u003cp\u003e7、歌尔股份公布业绩快报，2019年实现净利润13.07亿元，同比增长50.59%；公司智能无线耳机和智能穿戴业务发展顺利，利润贡献增加。\u003c/p\u003e\n\u003cp\u003e8、吉宏股份发布业绩快报，2019年净利润为3.24亿 同比增长52.27%。拟每10股转增7股，并派息1元。\u003c/p\u003e\n\u003cp\u003e9、北上资金昨日净流出67.27亿元。沪股通方面，中国平安净卖出7.60亿，恒瑞医药净卖出2.82亿。深股通方面，京东方A净卖出4.87亿，中兴通讯净卖出3.99亿。\u003c/p\u003e\n\u003cp\u003e10、兆易创新公告，因个人资金需求，4位高管计划减持7.67万股，即不超过0.0239%公司股份。\u003c/p\u003e\n\u003cp\u003e\u003cstrong\u003e环球市场：\u003c/strong\u003e\u003c/p\u003e\n\u003cp\u003e美股冲高回落 三大股指收盘涨跌不一\u003c/p\u003e\n\u003cp\u003e美东时间周三，美股冲高回落，三大股指收盘涨跌不一，其中，道指跌逾120点或0.46%，跌破27000点关口，盘中一度涨逾400点；纳指涨0.17%；标普500指数跌0.38%，为连续第5天下跌，创8月份以来最长连跌。COMEX 4月黄金期货收跌6.9美元，跌幅为0.42%。美油布油双双收跌超2%，WTI 4月原油期货收跌2.34%，布伦特4月原油期货收跌2.77%。\u003c/p\u003e\n\u003cp\u003e\u003cstrong\u003e二、【投资机会参考】\u003c/strong\u003e\u003c/p\u003e\n\u003cp\u003e\u003cstrong\u003e1、【多地出台重大投资计划 重点项目试点范围将扩大】\u003c/strong\u003e\u003c/p\u003e\n\u003cp\u003e交通运输部部长李小鹏主持召开部务会暨部应对新冠肺炎疫情工作领导小组会议、联防联控机制会议、复工复产机制会议，会议强调，要加快建设交通强国。统筹推进各项规划编制工作，推进重点工程建设项目，积极实施交通强国试点工作，进一步扩大试点范围。\u003c/p\u003e\n\u003cp\u003e近日，北京市、福建省、河南省、云南省、江苏省等地发布了2020年重大项目投资计划清单，总投资额合计逾11万亿元，其中基建投资仍是重要部分。日前，中共中央政治局会议再次提出要积极扩大内需，稳定外需，加快推动建设一批重大项目。机构认为，政策已明确在一方面抓紧防控疫情的情况下，一方面抓紧经济建设基建将成为主要的逆周期调节手段，全面复工后，有望看到基建投资明显增长。\u003c/p\u003e\n\u003cp\u003eA股上市公司中，福建水泥（600802）福建地区产能规模最大的水泥制造企业，具备年产能熟料784.3万吨、水泥1250万吨。徐工机械（000425）压路机、沥青混凝土摊铺机、平地机等国内市场占有率第一。\u003c/p\u003e\n\u003cp\u003e\u003cstrong\u003e2、【小米参投芯片企业 资本助力科技“飞向寻常百姓家”】\u003c/strong\u003e\u003c/p\u003e\n\u003cp\u003e25日，小米旗下湖北小米长江产业基金成为翱捷科技（上海）有限公司新增股东，新增股东还包括兴证投资、久有基金、弘信资本等。翱捷科技致力于移动智能通讯终端、物联网、导航及其他消费类电子芯片的平台研发、方案提供、技术支持和服务等，阿里巴巴和深圳创投早于2017年4月入资翱捷科技。消费电子上游芯片作为增速最快的细分市场，巨头参投如火如荼。\u003c/p\u003e\n\u003cp\u003e小米参投消费电子芯片并不是首次，2019年11月，小米旗下湖北小米长江产业基金就入股了物联网智能硬件提供核心芯片的芯片设计企业安凯微电子。产业趋势日渐明确，消费电子上游芯片市场星光夺目。分析认为，5G时代物联网/VR/AR应用拉动TOF需求，应用于TOF的半导体激光器VCSEL需求将进一步激增。自去年苹果推出iPhoneX后，整个VCSEL产业供应链紧俏，国内VCSEL供应商如纵慧、睿熙、华芯等突破显著。\u003c/p\u003e\n\u003cp\u003e相关上市公司中，华西股份（000936）参股常州纵慧芯光半导体14.67%股份，纵慧芯光是华为Mate30ProTOF摄像头零部件VCSEL的主供厂商。鲁信创投（600783）间接参股山东华芯半导体33.33%股份，华芯半导体自主完成垂直腔面发射激光器VCSEL和蓝光半导体激光器芯片外延及芯片工艺制造，已实现量产。\u003c/p\u003e\n\u003cp\u003e\u003cstrong\u003e3、【开启海外社交媒体时代 TikTok逆市高增长】\u003c/strong\u003e\u003c/p\u003e\n\u003cp\u003eSensor Tower商店情报数据显示，抖音及其海外版TikTok在过去4个月进入新一轮快速增长通道。今年1月，该应用在全球App Store和Google Play的下载量达到1.04亿次，超越WhatsApp，是全球下载量最高的移动应用。1月，抖音及TikTok下载量前三的市场为印度，巴西和美国，分别占34.4%，10.4%和7.3%。\u003c/p\u003e\n\u003cp\u003eTikTok避开了典型的中国软实力形式，它将带来一种更微妙的算法影响力形式，复杂的中国人工智能控制着可能在数百万美国年轻人之间传播的内容。这与Facebook、谷歌和Twitter在过去十年中所发挥的全球影响力没有什么不同。因此，外媒认为，硅谷或许已经开启了社交媒体时代，但它的未来可能在中国。\u003c/p\u003e\n\u003cp\u003e公司方面，久其软件（002279）帮助Tiktok在海外投广告获取用户，同时公司也是他们的一级代理商，帮助其他客户在平台上进行广告投放。吉宏股份（002803）精准营销广告业务与字节跳动保持良好的合作关系，公司的跨境电商业务也通过字节跳动的海外渠道Tiktok开拓业务。\u003c/p\u003e\n\u003cp\u003e\u003cstrong\u003e4、【索尼Vision-S电动汽车已开始进行道路测试 相关产业链受关注】\u003c/strong\u003e\u003c/p\u003e\n\u003cp\u003e索尼在2020年国际消费电子展发布了新型互联汽车平台Vision-S原型车。据外媒报道目前这辆车已在奥地利参加了公开的路面测试。不过，目前尚不清楚索尼正在进行的测试是否是为了验证自家发布的自动驾驶汽车技术。据悉，这辆车是由索尼的AI和机器人团队制造的，索尼的机器狗产品Aibo也由这两个团队开发。\u003c/p\u003e\n\u003cp\u003e近日，11部委联合下发《智能汽车创新发展战略》的通知，提出到2025年中国标准智能汽车的技术创新、产业生态、基础设施、法规标准、产品监管和网络安全体系基本形成。实现有条件自动驾驶的智能汽车达到规模化生产，实现高度自动驾驶的智能汽车在特定环境下市场化应用。展望2035到2050年，中国标准智能汽车体系全面建成、更加完善。\u003c/p\u003e\n\u003cp\u003eA股上市公司中，高新兴（300098）子公司中兴物联专注于物联网企业级市场，覆盖物联网无线通信模块，车联网产品，物联网行业终端及物联网通信管理平台与行业整体解决方案等四大业务板块。四创电子（600990）作为“中国雷达第一股”，主营业务涉及雷达、公共安全、卫星导航、微波通信、广播电视及智能交通等领域。\u003c/p\u003e",
+          "brief":"①基金公司表示并未有限制规模等约束性要求②美股冲高回落，科技股多数收涨③商务部：正在研究疫情结束后振兴餐饮业的措施④日韩新冠肺炎疫情持续扩大，韩国确诊超千人。",
+          "type":0,"sub_titles":null,"id":448195,"cate_name":"","recommend":0,"plates":null,"collection":0},"img":"","jpush":0,"bold":0,"comment_num":0,
+          "calendar_pic":"","deny_comment":0,"shareurl":"","ctime":1582758000,"sort_score":1582758000,"showurl":"","status":1,
+          "reading_num":675269,"title":"财联社2月27日早报（周四）","tags":[],"brief":"","type":0,"id":448195,"recommend":0,"show":true},
+
+          {"neican_img":"","calendar_color":"","morningNewsContent":null,"img":"","jpush":0,"bold":0,"comment_num":1,"calendar_pic":"","deny_comment":0,
+          "shareurl":"","ctime":1582671600,"sort_score":1582671600,"showurl":"","status":1,"reading_num":1186555,"title":"财联社2月26日早报（周三）",
+          "tags":[],"brief":"","type":0,"id":447472,"recommend":0,"show":false},{"neican_img":"","calendar_color":"","morningNewsContent":null,"img":"",
+          "jpush":0,"bold":0,"comment_num":1,"calendar_pic":"","deny_comment":0,"shareurl":"","ctime":1582585200,"sort_score":1582585200,"showurl":"",
+          "status":1,"reading_num":1167761,"title":"财联社2月25日早报（周二）","tags":[],"brief":"","type":0,"id":446765,"recommend":0,"show":false},
+
+          {"neican_img":"","calendar_color":"","morningNewsContent":null,"img":"","jpush":0,"bold":0,"comment_num":3,"calendar_pic":"",
+          "deny_comment":0,"shareurl":"","ctime":1582498800,"sort_score":1582498800,"showurl":"","status":1,"reading_num":1716086,
+          "title":"财联社2月24日早报（周一）","tags":[],"brief":"","type":0,"id":446060,"recommend":0,"show":false},
+
+          {"neican_img":"","calendar_color":"","morningNewsContent":null,"img":"","jpush":0,"bold":0,"comment_num":0,"calendar_pic":"",
+          "deny_comment":0,"shareurl":"","ctime":1582239600,"sort_score":1582239600,"showurl":"","status":1,"reading_num":2227435,
+          "title":"财联社2月21日早报（周五）","tags":[],"brief":"","type":0,"id":445062,"recommend":0,"show":false},
+
+          {"neican_img":"","calendar_color":"","morningNewsContent":null,"img":"","jpush":0,"bold":0,"comment_num":0,"calendar_pic":"",
+          "deny_comment":0,"shareurl":"","ctime":1582153200,"sort_score":1582153200,"showurl":"","status":1,"reading_num":1735397,"title":"财联社2月20日早报（周四）",
+          "tags":[],"brief":"","type":0,"id":444466,"recommend":0,"show":false},
+
+          {"neican_img":"","calendar_color":"","morningNewsContent":null,"img":"","jpush":0,"bold":0,
+          "comment_num":0,"calendar_pic":"","deny_comment":0,"shareurl":"",
+          "ctime":1582066800,"sort_score":1582066800,"showurl":"","status":1,"reading_num":1404856,
+          "title":"财联社2月19日早报（周三）","tags":[],"brief":"","type":0,"id":443869,"recommend":0,"show":false},
+
+
+          {"neican_img":"","calendar_color":"","morningNewsContent":null,"img":"","jpush":0,"bold":0,"comment_num":0,
+          "calendar_pic":"","deny_comment":0,"shareurl":"","ctime":1581980400,"sort_score":1581980400,"showurl":"",
+          "status":1,"reading_num":1973519,"title":"财联社2月18日早报（周二）","tags":[],"brief":"","type":0,"id":443282,"recommend":0,"show":false},
+
+          {"neican_img":"","calendar_color":"","morningNewsContent":null,"img":"","jpush":0,"bold":0,"comment_num":0,"calendar_pic":"","deny_comment":0,"shareurl":"","ctime":1581894000,"sort_score":1581894000,"showurl":"","status":1,"reading_num":1728107,"title":"财联社2月17日早报（周一）","tags":[],"brief":"","type":0,"id":442716,"recommend":0,"show":false},
+          {"neican_img":"","calendar_color":"","morningNewsContent":null,"img":"","jpush":0,"bold":0,"comment_num":0,"calendar_pic":"","deny_comment":0,"shareurl":"","ctime":1581634800,"sort_score":1581634800,"showurl":"","status":1,"reading_num":1225837,"title":"财联社2月14日早报（周五）","tags":[],"brief":"","type":0,"id":441704,"recommend":0,"show":false}],
+
+          "threeGuessList":[],"forecastList":[],"afterAfficheList":[],"bigLookTapeList":[],"networkNewsList":[],"csrcList":[]},
+
+          "our":{"navKey":"our","feedBack":{"data":{"content":"","username":"","phone":""},"error":""},
+          "clue":{"content":"","error":""},"page":"","allTelegraphList":{},"allThemeList":{},"allDepthList":{}},
+
+          "vip":{"columnDetail":{},"error":{},"vipNotificationState":"on","bannerList":[],"subscribedColumn":[],"articleZan":{},
+          "trialList":[],"columnList":[],"getMoreButtonState":"finish","subscribedList":[],"recommendList":[],"newSubscribedArticle":[],
+          "columnArticleList":[]},"details":{"weixinCodeUrl":"","weixinCodeStatus":false,"errno":0,"currentComment":null,"commentNum":null,
+          "commentError":"","dianZanComment":{"id":0,"zan":false},"replyContent":"","queryData":{},"commentValue":"","commentBtnState":true,
+          "errorMsg":"","content":{},"explains":[],"comment":[]},"searchPage":{"searchValue":"","telegraphData":{"data":{}},"depthData":{"data":{}},
+          "themeData":{"data":{}},"referenceData":{"data":{}},"featuredData":{"data":{}},"list":{"data":{},"page":0,"buttonState":"over"}},
+          "famous":{"dataList":{},"videoDetail":{},"errno":0}},"initialProps":{}},
+          "page":"/reference","pathname":"/reference","query":{"type":"1"},"buildId":"e23b0ac4-4c38-48af-a2b1-502d2df22542",
+          "assetPrefix":"https://wwwjs.cls.cn","nextExport":false,"err":null,"chunks":[]}
+          module={}
+          __NEXT_LOADED_PAGES__ = []
+          __NEXT_LOADED_CHUNKS__ = []
+
+          __NEXT_REGISTER_PAGE = function (route, fn) {
+            __NEXT_LOADED_PAGES__.push({ route: route, fn: fn })
+          }
+
+          __NEXT_REGISTER_CHUNK = function (chunkName, fn) {
+            __NEXT_LOADED_CHUNKS__.push({ chunkName: chunkName, fn: fn })
+          }
+
+          false
+
+
+
+        <script async="" id="__NEXT_PAGE__/reference" src="https://wwwjs.cls.cn/_next/e23b0ac4-4c38-48af-a2b1-502d2df22542/page/reference.js"></script>
+        <script async="" id="__NEXT_PAGE__/_app" src="https://wwwjs.cls.cn/_next/e23b0ac4-4c38-48af-a2b1-502d2df22542/page/_app.js"></script>
+        <script async="" id="__NEXT_PAGE__/_error" src="https://wwwjs.cls.cn/_next/e23b0ac4-4c38-48af-a2b1-502d2df22542/page/_error.js"></script>
+        <script src="https://wwwjs.cls.cn/_next/static/commons/main-1ffe918ad7673c8f75f3.js" async=""></script>
+    </body>
+    <script async="" src="https://cdnjs.cls.cn/libs/web/global.js"></script>
+</html>
+
