fb63a05a849c03b16bd22b9af10c6550e1e10a58
furuiyang <furuiyang0715@gmail.com>
2020-02-27 09:11:30 +0800

    财联社基类

--- PublicOpinion/cls_cn/cls_base.py
+++ PublicOpinion/cls_cn/cls_base.py
@@ -1,35 +1,23 @@
-# https://www.cls.cn/nodeapi/depths?last_time={}&refreshType=1&rn=20&sign=900569309a173964ce973dc61bbc2455
 import datetime
-import json
-import random
 import re
-import sys
-import time
 import traceback
 
 import pymysql
-import requests
 
 from PublicOpinion.configs import LOCAL, LOCAL_MYSQL_HOST, LOCAL_MYSQL_PORT, LOCAL_MYSQL_USER, LOCAL_MYSQL_PASSWORD, \
     LOCAL_MYSQL_DB, MYSQL_HOST, MYSQL_PORT, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB
 from PublicOpinion.sql_pool import PyMysqlPoolBase
 
-now = lambda: int(time.time())
 
-
-class Depth(object):
+class ClsBase(object):
+    # 财联社-基类
     def __init__(self):
         self.local = LOCAL
         self.this_last_dt = None
-        # self.items = []
-        self.name = '财联社-深度及题材'
-        # self.url_format = 'https://www.cls.cn/nodeapi/telegraphs?refresh_type=1&rn=20&last_time={}&sign=56918b10789cb8a977c518409e7f0ced'
-        self.url_format = 'https://www.cls.cn/nodeapi/depths?last_time={}&refreshType=1&rn=20&sign=900569309a173964ce973dc61bbc2455'
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, '
                           'like Gecko) Chrome/79.0.3945.117 Safari/537.36'
         }
-        self.table = 'cls_depth_theme'
 
     def _init_pool(self):
         if self.local:
@@ -50,93 +38,9 @@ class Depth(object):
             }
         self.sql_pool = PyMysqlPoolBase(**conf)
 
-    def _parse_detail(self):
-        pass
-
-    def refresh(self, url):
-        resp = requests.get(url, headers=self.headers)
-        if resp.status_code == 200:
-            py_data = json.loads(resp.text)
-            # print(py_data)
-            # sys.exit(0)
-            infos = py_data.get("data")
-            print(infos)
-            if not infos:
-                return
-            items = []
-            for info in infos:
-                item = {}
-                title = info.get("title")
-                if not title:
-                    title = info.get("content")[:20]
-                item['title'] = title
-                pub_date = info.get("ctime")
-
-                article_id = info.get("article_id")
-                item['link'] = "https://www.cls.cn/depth/{}".format(article_id)
-
-                item['pub_date'] = self.convert_dt(pub_date)
-                # item['article'] = self._parse_detail(item['link'])
-                items.append(item)
-            # self.save(items)
-            print(items)
-
-            dt = infos[-1].get('ctime')
-            if dt == self.this_last_dt:
-                print("增量完毕 .. ")
-                return
-            self.this_last_dt = dt
-            # dt - 1 是为了防止临界点重复值 尽量 insert_many 成功。
-            next_url = self.url_format.format(dt-1)
-            time.sleep(random.randint(1, 3))
-            print("next_url: ", next_url)
-            self.refresh(next_url)
-
-    def save(self, items):
-        ret = self._save_many(items)
-        if not ret:
-            print("批量保存失败 开始单独保存 .. ")
-            count = 0
-            for item in items:
-                print(item)
-                self._save(item)
-                count += 1
-                if count > 9:
-                    self.sql_pool.end()
-                    count = 0
-            self.sql_pool.dispose()
-        else:
-            print("批量成功..")
-            print(items)
-            print(len(items))
-
-    def _start(self):
-        self._init_pool()
-        first_url = self.url_format.format(now())
-        self.refresh(first_url)
-
     def convert_dt(self, time_stamp):
         d = datetime.datetime.fromtimestamp(time_stamp)
         return d
-
-    def _create_table(self):
-        create_sql = '''
-        CREATE TABLE IF NOT EXISTS `cls_depth_theme`(
-          `id` int(11) NOT NULL AUTO_INCREMENT,
-          `pub_date` datetime NOT NULL COMMENT '发布时间',
-          `title` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '文章标题',
-          `link` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '文章详情页链接',
-          `article` text CHARACTER SET utf8 COLLATE utf8_bin COMMENT '详情页内容',
-          `CREATETIMEJZ` datetime DEFAULT CURRENT_TIMESTAMP,
-          `UPDATETIMEJZ` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-          PRIMARY KEY (`id`),
-          UNIQUE KEY `link` (`link`),
-          KEY `pub_date` (`pub_date`)
-        ) ENGINE=InnoDB AUTO_INCREMENT=34657 DEFAULT CHARSET=utf8mb4 COMMENT='财联社-深度及题材' ; 
-        '''
-        ret = self.sql_pool._exec_sql(create_sql)
-        self.sql_pool.end()
-        return ret
 
     def _contract_sql(self, to_insert):
         ks = []
@@ -210,13 +114,26 @@ class Depth(object):
         finally:
             self.sql_pool.end()
 
+    def save(self, items):
+        ret = self._save_many(items)
+        if not ret:
+            print("批量保存失败 开始单独保存 .. ")
+            count = 0
+            for item in items:
+                print(item)
+                self._save(item)
+                count += 1
+                if count > 9:
+                    self.sql_pool.end()
+                    count = 0
+            self.sql_pool.dispose()
+        else:
+            print("批量成功..")
+            print(items)
+            print(len(items))
+
     def __del__(self):
         try:
             self.sql_pool.dispose()
         except:
             pass
-
-
-if __name__ == "__main__":
-    d = Depth()
-    d._start()
