38194a8049fb26cbe1119abea7bafbed96ee9494
furuiyang <furuiyang0715@gmail.com>
2020-02-29 15:22:42 +0800

    财新社监控

--- PublicOpinion/cls_cn/__init__.py
+++ PublicOpinion/cls_cn/__init__.py
@@ -1,1 +1,7 @@
 # 财联社
+
+
+# 监控:
+# SELECT count(id) FROM cls_afterAfficheList WHERE pub_date > date_sub(CURDATE(), interval 1 day);
+# SELECT count(id) FROM cls_depth_theme WHERE pub_date > date_sub(CURDATE(), interval 1 day);
+# SELECT count(id) FROM cls_telegraphs  WHERE pub_date > date_sub(CURDATE(), interval 1 day);
