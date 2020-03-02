fbc58d0c6c98d984912eb2d143f48fcd0792f0a0
furuiyang <furuiyang0715@gmail.com>
2020-03-02 15:33:08 +0800

    公司要闻

---
+++ PublicOpinion/takungpao/hkstock_gsyw.py
@@ -0,0 +1,15 @@
+from PublicOpinion.takungpao.hkstock_cjss import HKStock_CJSS
+
+
+class HKStock_GSYW(HKStock_CJSS):
+
+    def __init__(self):
+        super(HKStock_GSYW, self).__init__()
+        self.name = '公司要闻'
+        self.first_url = 'http://finance.takungpao.com/hkstock/gsyw/index.html'
+        self.format_url = "http://finance.takungpao.com/hkstock/gsyw/index_{}.html"
+
+
+if __name__ == "__main__":
+    cjss = HKStock_GSYW()
+    cjss.start()
