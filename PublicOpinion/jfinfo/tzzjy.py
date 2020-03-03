365d121838282ed5c82f64c6e19bd643f384bf09
furuiyang <furuiyang0715@gmail.com>
2020-03-03 16:37:06 +0800

    投资者教育

---
+++ PublicOpinion/jfinfo/tzzjy.py
@@ -0,0 +1,17 @@
+# coding=utf8
+
+from PublicOpinion.jfinfo.reference import Reference
+
+
+class TZZJY(Reference):
+    def __init__(self):
+        super(TZZJY, self).__init__()
+        self.index_url = 'http://www.jfinfo.com/reference/tzzjy'
+        self.more_url = 'http://www.jfinfo.com/articles_categories/more?page={}&category_id=59'
+        self.max_page = 20
+        self.name = '投资者教育'
+
+
+if __name__ == "__main__":
+    runner = TZZJY()
+    runner.start()
