938b66e201b43c24d436108dee442ecbb6e70d6a
furuiyang <furuiyang0715@gmail.com>
2020-04-23 11:00:07 +0800

    插入

--- my_stock/load_csv.py
+++ my_stock/load_csv.py
@@ -3,11 +3,6 @@ import datetime
 import pymongo
 import pandas as pd
 
-file = '/Users/furuiyang/gitzip/JustSimpleSpider/my_stock/buy.txt'
-df = pd.read_csv(file, encoding='utf-8')
-datas = df.to_dict('records')
-
-
 code_map = {
     "螺丝钉指数基金组合(CSI666)": 'CSI666',
     "华安德国30(DAX)ETF连接": '000614',
@@ -23,27 +18,49 @@ code_map = {
     "大成纳斯达克100指数": "000834",
     "嘉实全球互联网股票": "000988",
     "交银中证海外中国互联网指数": "164906",
-
-
-
+    "博时标普500ETF联接": "050025",
+    "招商中证白酒指数": "161725",
+    "广发中证环保ETF联接A": "001064",
 
 }
 
 
-mongo_client = pymongo.MongoClient("127.0.0.1:27017")
-fund_cli = mongo_client.myfund.records
-for data in datas:
-    print(data)
-    item = dict()
-    item['time'] = datetime.datetime.strptime(data.get("时间"), "%Y/%m/%d")
-    item['direction'] = 1 if data.get("方向") == "买入" else 2
-    item['amount'] = float(data.get("金额"))
-    item['fund_name'] = data.get("类别")
-    item['code'] = code_map.get(data.get("类别"))
-    item['app'] = "蛋卷基金"
+def read_csv():
+    file = '/Users/furuiyang/gitzip/JustSimpleSpider/my_stock/buy.txt'
+    df = pd.read_csv(file, encoding='utf-8')
+    datas = df.to_dict('records')
+    return datas
 
 
+def process_datas(datas):
+    items = list()
+    for data in datas:
+        # print(data)
+        item = dict()
+        item['time'] = datetime.datetime.strptime(data.get("时间"), "%Y/%m/%d")
+        item['direction'] = 1 if data.get("方向") == "买入" else 2
+        item['amount'] = float(data.get("金额"))
+        item['fund_name'] = data.get("类别")
+        item['code'] = code_map.get(data.get("类别"))
+        item['app'] = "蛋卷基金"
+        if not item['code']:
+            raise Exception("没有对应的基金代码")
+        # print(item)
+        items.append(item)
+    return items
 
 
+def insert_to_mongo(items):
+    mongo_client = pymongo.MongoClient("127.0.0.1:27017")
+    fund_cli = mongo_client.myfund.records
+    ret = fund_cli.insert_many(items)
+    print(ret)
 
-# demo = {"time": "", "name": "", "direction": "buy", "amount": "", "code": "", "app": ""}
+
+def main():
+    datas = read_csv()
+    items = process_datas(datas)
+    insert_to_mongo(items)
+
+
+main()
