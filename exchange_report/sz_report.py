177a87bea3b71d843097b7996d9bd8bc1c09b726
furuiyang <furuiyang0715@gmail.com>
2020-05-14 14:08:17 +0800

    建表

--- exchange_report/sz_report.py
+++ exchange_report/sz_report.py
@@ -1,18 +1,44 @@
 import xlrd
 
+from exchange_report.base import ReportBase
 
-class SZReport(object):
+
+class SZReport(ReportBase):
     """深交所行情爬虫"""
     def __init__(self):
-        self.fields = ['TradingDay', 'SecuCode', 'SecuAbbr', 'PrevClose', 'Close', 'RiseFall', 'Amount', 'PERatio']
+        super(SZReport, self).__init__()
+        self.fields = ['TradingDay', 'SecuCode', 'InnerCode', 'SecuAbbr', 'PrevClose', 'Close',
+                       'RiseFall', 'Amount', 'PERatio']
+        self.table_name = 'szse_dailyquote'
 
     def _create_table(self):
-
-        pass
+        sql = '''
+        CREATE TABLE IF NOT EXISTS `{}` (
+          `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+          `SecuCode` varchar(10) DEFAULT NULL COMMENT '证券代码',
+          `InnerCode` int(11) NOT NULL COMMENT '证券内部编码',  
+          `SecuAbbr` varchar(100) DEFAULT NULL COMMENT '证券简称',
+          `TradingDay` datetime NOT NULL COMMENT '交易日',
+          `PrevClose` decimal(10,4) DEFAULT NULL COMMENT '前收价(元)',
+          `Amount` decimal(19,4) DEFAULT NULL COMMENT '成交金额(元)',
+          `RiseFall` decimal(10,4) DEFAULT NULL COMMENT '涨跌',
+          `PERatio` decimal(10,4) DEFAULT NULL COMMENT '市盈率',
+          `CREATETIMEJZ` datetime DEFAULT CURRENT_TIMESTAMP,
+          `UPDATETIMEJZ` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, 
+           UNIQUE KEY `IX_QT_DailyQuote` (`InnerCode`,`TradingDay`),
+           UNIQUE KEY `PK_QT_DailyQuote` (`ID`),
+           KEY `IX_QT_DailyQuote_TradingDay` (`TradingDay`)
+           ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='交易所行情';
+        '''.format(self.table_name)
+        client = self._init_pool(self.spider_cfg)
+        client.insert(sql)
+        client.dispose()
 
     def start(self):
+        self._create_table()
+
         wb = xlrd.open_workbook('股票行情.xlsx')
-        sheet_names = wb.sheet_names()
+        # sheet_names = wb.sheet_names()
         ws = wb.sheet_by_name('股票行情')
         _rows = ws.nrows
         for idx in range(0, _rows):
