d45dfbe0ac6fdd94056ece758e989461d6ea4629
furuiyang <furuiyang0715@gmail.com>
2020-07-06 17:33:13 +0800

    add some

--- Takungpao/hkstock_cjss.py
+++ Takungpao/hkstock_cjss.py
@@ -1,20 +1,15 @@
-from gne import GeneralNewsExtractor
 from lxml import html
+from Takungpao.base import TakungpaoBase
+from base import logger
 
-from takungpao.base import Base
 
-
-class HKStock_CJSS(Base):
-
+class HKStock_CJSS(TakungpaoBase):
     def __init__(self):
         super(HKStock_CJSS, self).__init__()
-        self.page = 11
+        self.page = 3
         self.name = '财经时事'
         self.first_url = 'http://finance.takungpao.com/hkstock/cjss/index.html'
         self.format_url = "http://finance.takungpao.com/hkstock/cjss/index_{}.html"
-        self.extractor = GeneralNewsExtractor()
-        self.table = 'Takungpao'
-        self.fields = ['link', 'title', 'pub_date', 'article']
 
     def _parse_detail(self, body):
         result = self.extractor.extract(body)
@@ -49,28 +44,42 @@ class HKStock_CJSS(Base):
             items.append(item)
 
             detail_resp = self.get(link)
-            if detail_resp:
+            if detail_resp and detail_resp.status_code == 200:
                 article = self._parse_detail(detail_resp.text)
                 if article:
                     article = self._process_content(article)
                     item['article'] = article
+                    print(item)
         return items
 
-    def _start(self):
+    def start(self):
+        self._create_table()
+        self._spider_init()
         for page in range(1, self.page+1):
-            print(">>> ", page)
             if page == 1:
                 list_url = self.first_url
             else:
                 list_url = self.format_url.format(page)
 
             list_resp = self.get(list_url)
-            if list_resp:
+            if list_resp and list_resp.status_code == 200:
                 items = self.parse_list(list_resp.text)
-                print(items)
-                self.save(items)
+                page_save_num = self._batch_save(self.spider_client, items, self.table_name, self.fields)
+                logger.info(f"第{page}页保存成功的个数{page_save_num}")
+
+
+class HKStock_QQGS(HKStock_CJSS):
+    def __init__(self):
+        super(HKStock_QQGS, self).__init__()
+        self.page = 3
+        self.name = '全球股市'
+        self.first_url = 'http://finance.takungpao.com/hkstock/qqgs/index.html'
+        self.format_url = "http://finance.takungpao.com/hkstock/qqgs/index_{}.html"
 
 
 if __name__ == "__main__":
-    cjss = HKStock_CJSS()
-    cjss.start()
+    # cjss = HKStock_CJSS()
+    # cjss.start()
+
+    qqgs = HKStock_QQGS()
+    qqgs.start()
