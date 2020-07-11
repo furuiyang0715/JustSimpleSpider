f4cc3ca11fd01cd2acef395ead6da54b10c93fe7
furuiyang <furuiyang0715@gmail.com>
2020-07-11 13:46:54 +0800

    主题调度

--- CArticle/ca_main.py
+++ CArticle/ca_main.py
@@ -1,80 +1,61 @@
-# import logging
-# import random
-# import time
-#
-# import pymysql
-# import os
-# from apscheduler.schedulers.blocking import BlockingScheduler
-#
-# from CArticle.spider import CArticleSpiser
-# from PublicOpinion.carticle.configs import DC_HOST, DC_PORT, DC_USER, DC_PASSWD, DC_DB
-#
-# logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-# logger = logging.getLogger(__name__)
-#
-# _now = lambda: time.time()
-#
-#
-# class Schedule(object):
-#     def __init__(self):
-#         self.keys = list(self.dc_info().values())
-#         random.shuffle(self.keys)
-#
-#     def dc_info(self):  # eg. {'300150.XSHE': '世纪瑞尔',
-#         """
-#         从 datacanter.const_secumain 数据库中获取当天需要爬取的股票信息
-#         返回的是 股票代码: 中文名简称 的字典的形式
-#         """
-#         try:
-#             conn = pymysql.connect(host=DC_HOST, port=DC_PORT, user=DC_USER,
-#                                    passwd=DC_PASSWD, db=DC_DB)
-#         except Exception as e:
-#             raise
-#
-#         cur = conn.cursor()
-#         cur.execute("USE datacenter;")
-#         cur.execute("""select SecuCode, ChiNameAbbr from const_secumain where SecuCode \
-#             in (select distinct SecuCode from const_secumain);""")
-#         dc_info = {r[0]: r[1] for r in cur.fetchall()}
-#         cur.close()
-#         conn.close()
-#         return dc_info
-#
-#     def start(self, key):
-#         c = CArticleSpiser(key=key)
-#         c.start()
-#
-#     def run(self):
-#         start_time = _now()
-#         for key in self.keys:
-#             logger.info("当前的主题是: {}".format(key))
-#             self.start(key)
-#         logger.info("一次共用时: {}".format(_now() - start_time))
-#
-#
-# def task():
-#     sche = Schedule()
-#     sche.run()
-#
-#
-# if __name__ == '__main__':
-#     scheduler = BlockingScheduler()
-#     task()
-#     scheduler.add_job(task, 'interval', hours=15)
-#     logger.info('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
-#
-#     try:
-#         scheduler.start()
-#     except (KeyboardInterrupt, SystemExit):
-#         pass
-#
-#
-#
-# '''
-# docker build -f Dockerfile.carticle -t registry.cn-shenzhen.aliyuncs.com/jzdev/jzdata/ca:v1 .
-# docker push registry.cn-shenzhen.aliyuncs.com/jzdev/jzdata/ca:v1
-# sudo docker pull registry.cn-shenzhen.aliyuncs.com/jzdev/jzdata/ca:v1
-# sudo docker run -itd --name dongc --env LOCAL=0 registry.cn-shenzhen.aliyuncs.com/jzdev/jzdata/ca:v1
-#
-# SELECT count(id) FROM  eastmoney_carticle  WHERE pub_date > date_sub(CURDATE(), interval 1 day);
-# '''
+import random
+
+from CArticle.ca_spider import CArticleSpiser
+from base import SpiderBase, logger
+
+
+class CaSchedule(SpiderBase):
+    table_name = "eastmoney_carticle"
+    dt_benchmark = 'pub_date'
+
+    def __init__(self):
+        super(CaSchedule, self).__init__()
+        self.keys = list(self.dc_info().values())
+        random.shuffle(self.keys)
+
+    def dc_info(self):
+        self._dc_init()
+        sql = '''select SecuCode, ChiNameAbbr from const_secumain where SecuCode \
+            in (select distinct SecuCode from const_secumain);'''
+        datas = self.dc_client.select_all(sql)
+        dc_info = {r['SecuCode']: r['ChiNameAbbr'] for r in datas}
+        return dc_info
+
+    def start(self, key):
+        CArticleSpiser(key=key).start()
+
+    def _create_table(self):
+        self._spider_init()
+        sql = '''
+           CREATE TABLE IF NOT EXISTS `{}` (
+             `id` int(11) NOT NULL AUTO_INCREMENT,
+             `pub_date` datetime NOT NULL COMMENT '发布时间',
+             `code` varchar(16) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '股票代码',
+             `title` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '文章标题',
+             `link` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '文章详情页链接',
+             `article` text CHARACTER SET utf8 COLLATE utf8_bin COMMENT '详情页内容',
+             `CREATETIMEJZ` datetime DEFAULT CURRENT_TIMESTAMP,
+             `UPDATETIMEJZ` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+             PRIMARY KEY (`id`),
+             UNIQUE KEY `link` (`link`),
+             KEY `pub_date` (`pub_date`),
+             KEY `update_time` (`UPDATETIMEJZ`)
+           ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='东财-财富号文章'; 
+           '''.format(self.table_name)
+        self.spider_client.insert(sql)
+        self.spider_client.end()
+
+    def run(self):
+        self._create_table()
+        for key in self.keys:
+            logger.info("当前的主题是: {}".format(key))
+            self.start(key)
+
+
+def task():
+    sche = CaSchedule()
+    sche.run()
+
+
+if __name__ == '__main__':
+    task()
--- CArticle/ca_spider.py
+++ CArticle/ca_spider.py
@@ -43,27 +43,6 @@ class CArticleSpiser(SpiderBase):
         }
         return query_params
 
-    def _create_table(self):
-        self._spider_init()
-        sql = '''
-        CREATE TABLE IF NOT EXISTS `{}` (
-          `id` int(11) NOT NULL AUTO_INCREMENT,
-          `pub_date` datetime NOT NULL COMMENT '发布时间',
-          `code` varchar(16) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '股票代码',
-          `title` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '文章标题',
-          `link` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '文章详情页链接',
-          `article` text CHARACTER SET utf8 COLLATE utf8_bin COMMENT '详情页内容',
-          `CREATETIMEJZ` datetime DEFAULT CURRENT_TIMESTAMP,
-          `UPDATETIMEJZ` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-          PRIMARY KEY (`id`),
-          UNIQUE KEY `link` (`link`),
-          KEY `pub_date` (`pub_date`),
-          KEY `update_time` (`UPDATETIMEJZ`)
-        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='东财-财富号文章'; 
-        '''.format(self.table_name)
-        self.spider_client.insert(sql)
-        self.spider_client.end()
-
     def _get_proxy(self):
         if LOCAL:
             return requests.get(LOCAL_PROXY_URL).text.strip()
@@ -141,7 +120,6 @@ class CArticleSpiser(SpiderBase):
             return None
 
     def start(self):
-        self._create_table()
         self._spider_init()
 
         items = []
