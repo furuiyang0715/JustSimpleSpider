841b2a3e774923b4a9b3926fa6649257dbb4f79a
furuiyang <furuiyang0715@gmail.com>
2020-07-13 14:42:20 +0800

    将函数作为参数传入

--- JustTest/Decoretor_test/simple_func.py
+++ JustTest/Decoretor_test/simple_func.py
@@ -16,8 +16,19 @@ def my_add_1(l1, l2):
     print(t2 - t1)
 
 
+def timer(func, *args):
+    """将函数作为参数传入去计算执行时间"""
+    start = time.time()
+    func(*args)
+    time.sleep(2)  # 模拟耗时操作
+    long = time.time() - start
+    print(f'共耗时{long}秒。')
+
+
 if __name__ == "__main__":
 
-    my_add(1, 2)
+    # my_add(1, 2)
+    #
+    # my_add_1(1000, 1000)
 
-    my_add_1(1000, 1000)
+    timer(my_add, 1000, 2000)
