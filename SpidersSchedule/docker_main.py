e632053baa508a26db95ee12db26051143e10b2d
furuiyang <furuiyang0715@gmail.com>
2020-07-22 16:13:06 +0800

    add

--- SpidersSchedule/docker_main.py
+++ SpidersSchedule/docker_main.py
@@ -3,6 +3,7 @@ import traceback
 import docker
 
 from base import logger
+from configs import LOCAL
 
 docker_client = docker.from_env()
 
@@ -16,31 +17,20 @@ def docker_run_spider(spider_name, spider_file_path, restart=False):
         spider_status = spider_container.status
         logger.info(f"{spider_name} spider status: {spider_status}")
         if spider_status in ("exited", ):
-            logger.info("任务已完成")
-            try:
-                spider_container.start()
-            except:
-                traceback.print_exc()
+            spider_container.start()
         elif spider_status in ("running", ):
-            logger.info("上次任务还未执行完")
             if restart:
-                try:
-                    spider_container.restart()
-                except:
-                    traceback.print_exc()
+                spider_container.restart()
         else:
-            logger.warning(f"其他状态 {spider_status}")
+            logger.warning(f"other status: {spider_status}")
     else:
-        try:
-            docker_containers_col.run(
-                "registry.cn-shenzhen.aliyuncs.com/jzdev/jzdata/spi:v1",
-                environment={"LOCAL": 1},
-                name=f'{spider_name}',
-                command=f'python {spider_file_path}',
-                detach=True,
-            )
-        except:
-            traceback.print_exc()
+        docker_containers_col.run(
+            "registry.cn-shenzhen.aliyuncs.com/jzdev/jzdata/spi:v1",
+            environment={"LOCAL": LOCAL},
+            name=f'{spider_name}',
+            command=f'python {spider_file_path}',
+            detach=True,
+        )
 
 
 def main():
