2207688085a66c641fcd201955ef8157715e0243
furuiyang <furuiyang0715@gmail.com>
2020-07-24 11:25:11 +0800

    bug fixed

--- SpidersSchedule/docker_main.py
+++ SpidersSchedule/docker_main.py
@@ -1,67 +1,212 @@
+import functools
 import os
+import pprint
 import sys
+import time
 import traceback
 
 import docker
+import schedule
 
 cur_path = os.path.split(os.path.realpath(__file__))[0]
 file_path = os.path.abspath(os.path.join(cur_path, ".."))
 sys.path.insert(0, file_path)
 
-from base import logger
-docker_client = docker.from_env()
-docker_containers_col = docker_client.containers
+from configs import LOCAL
+from base import logger, SpiderBase
 
 
-def docker_run_spider(spider_name, spider_file_path, restart=False):
-    try:
-        spider_container = docker_containers_col.get(spider_name)
-    except:
-        spider_container = None
+def catch_exceptions(cancel_on_failure=False):
+    def catch_exceptions_decorator(job_func):
+        @functools.wraps(job_func)
+        def wrapper(*args, **kwargs):
+            try:
+                return job_func(*args, **kwargs)
+            except:
+                logger.warning(traceback.format_exc())
+                # sentry.captureException(exc_info=True)
+                if cancel_on_failure:
+                    logger.warning("异常 任务结束: {}".format(schedule.CancelJob))
+                    schedule.cancel_job(job_func)
+                    return schedule.CancelJob
+        return wrapper
+    return catch_exceptions_decorator
 
-    if spider_container:
-        spider_status = spider_container.status
-        logger.info(f"{spider_name} spider status: {spider_status}")
-        if spider_status in ("exited", ):
-            spider_container.start()
-        elif spider_status in ("running", ):
-            if restart:
-                spider_container.restart()
+
+# docker_client = docker.from_env()
+# docker_containers_col = docker_client.containers
+# def docker_run_spider(spider_name, spider_file_path, restart=False):
+#     try:
+#         spider_container = docker_containers_col.get(spider_name)
+#     except:
+#         spider_container = None
+#
+#     if spider_container:
+#         spider_status = spider_container.status
+#         logger.info(f"{spider_name} spider status: {spider_status}")
+#         if spider_status in ("exited", ):
+#             spider_container.start()
+#         elif spider_status in ("running", ):
+#             if restart:
+#                 spider_container.restart()
+#         else:
+#             logger.warning(f"other status: {spider_status}")
+#     else:
+#         docker_containers_col.run(
+#             "registry.cn-shenzhen.aliyuncs.com/jzdev/jzdata/spi:v1",
+#             environment={"LOCAL": 1},
+#             name=f'{spider_name}',
+#             command=f'python {spider_file_path}',
+#             detach=True,
+#         )
+# def main():
+#     docker_run_spider("test_sohu", 'sohu/sohu_spider.py')
+#     docker_run_spider("calander_news", 'CalendarNewsRelease/news_release.py')
+#     docker_run_spider("dongc", 'CArticle/ca_main.py')
+#     docker_run_spider("cls", 'ClsCnInfo/telegraphs.py')
+#     docker_run_spider("cls2", 'ClsCnInfo/cls_details.py')
+#     docker_run_spider("gov", 'GovSpiders/gov_main.py')
+#     docker_run_spider('juf', 'JfInfo/jfinfo_main.py')
+#     docker_run_spider('juc', 'JuchaoInfo/juchao.py')
+#     docker_run_spider('m163', 'Money163/netease_money.py')
+#     docker_run_spider('qqf', 'QQStock/qq_stock.py')
+#     docker_run_spider('shcn', 'ShangHaiSecuritiesNews/cn_main.py')
+#     docker_run_spider('sohu', 'sohu/sohu_spider.py')
+#     docker_run_spider('stcn', 'StockStcn/kuaixun.py')
+#     docker_run_spider('tkg', 'Takungpao/takungpao_main.py')
+#     docker_run_spider('tgb', 'Taoguba/tgb_main.py')
+#     docker_run_spider('yicai', 'YiCai/yicai_spider.py')
+#
+
+
+class DockerSwith(SpiderBase):
+    def __init__(self):
+        super(DockerSwith, self).__init__()
+        self.tables = list()
+        self.docker_client = docker.from_env()
+        self.docker_containers_col = self.docker_client.containers
+
+    def ding_crawl_information(self):
+        self._spider_init()
+        print(self.tables)
+        msg = ''
+        for table, dt_benchmark in self.tables:
+            sql = '''SELECT count(id) as inc_count FROM {} WHERE {} > date_sub(CURDATE(), interval 1 day);'''.format(table, dt_benchmark)
+            inc_count = self.spider_client.select_one(sql).get("inc_count")
+            msg += f'{table} 今日新增 {inc_count}\n'
+
+        if not LOCAL:
+            self.ding(msg)
         else:
-            logger.warning(f"other status: {spider_status}")
-    else:
-        docker_containers_col.run(
-            "registry.cn-shenzhen.aliyuncs.com/jzdev/jzdata/spi:v1",
-            environment={"LOCAL": 1},
-            name=f'{spider_name}',
-            command=f'python {spider_file_path}',
-            detach=True,
-        )
+            print(msg)
 
+    def show_spider_logs(self, spider_name, lines=100):
+        spider_container = self.docker_containers_col.get(spider_name)
+        log_info = spider_container.logs(tail=lines).decode()
+        return log_info
 
-def show_spider_logs(spider_name, lines=100):
-    spider_container = docker_containers_col.get(spider_name)
-    log_info = spider_container.logs(tail=lines).decode()
-    return log_info
+    def docker_run_spider(self, spider_name, spider_file_path, restart=False):
+        try:
+            spider_container = self.docker_containers_col.get(spider_name)
+        except:
+            spider_container = None
+
+        if spider_container:
+            spider_status = spider_container.status
+            logger.info(f"{spider_name} spider status: {spider_status}")
+            if spider_status in ("exited",):
+                spider_container.start()
+            elif spider_status in ("running",):
+                if restart:
+                    spider_container.restart()
+            else:
+                logger.warning(f"other status: {spider_status}")
+        else:
+            self.docker_containers_col.run(
+                "registry.cn-shenzhen.aliyuncs.com/jzdev/jzdata/spi:v1",
+                environment={"LOCAL": 1},
+                name=f'{spider_name}',
+                command=f'python {spider_file_path}',
+                detach=True,
+            )
+
+    def start_task(self, spider_name, spider_file_path, table_name, dt_benchmark, dt_str, restart=False):
+        """
+        使用定时任务每天固定时间启动 docker 进程
+        :param spider_name: 爬虫名称/容器名
+        :param spider_file_path: 爬虫文件路径
+        :param table_name: 爬虫数据存放数据库表名
+        :param dt_benchmark: 爬虫增量字段 例如 pub_date 则是以发布时间来衡量两个点之间的数据增量
+        :param dt_str: 定时任务时间字符串
+        :param restart: 如果前一个任务的容器还在运行 是否进行重启
+        :return:
+        """
+        @catch_exceptions(cancel_on_failure=True)
+        def task():
+            self.docker_run_spider(spider_name, spider_file_path, restart)
+
+        self.tables.append((table_name, dt_benchmark))
+        schedule.every().day.at(dt_str).do(task)
+
+    def interval_start_task(self, spider_name, spider_file_path, table_name, dt_benchmark, interval: tuple, restart=False):
+        """
+        使用定时任务固定间隔启动 docker 进程
+        :param spider_name:
+        :param spider_file_path:
+        :param table_name:
+        :param dt_benchmark:
+        :param interval: 元组（间隔时间, 间隔单位）eg. (10, "minutes"), 每 10 分钟运行一次
+        :param restart:
+        :return:
+        """
+        @catch_exceptions(cancel_on_failure=True)
+        def task():
+            self.docker_run_spider(spider_name, spider_file_path, restart)
+
+        self.tables.append((table_name, dt_benchmark))
+        sche = schedule.every(interval[0])
+        assert interval[1] in ('seconds', 'minutes', 'hours', 'days', 'weeks')
+        sche.unit = interval[1]
+        sche.do(task)
+
+    def run(self):
+        # # 交易所日历公告爬虫 / 每天爬取 1 次
+        # self.docker_run_spider("calander_news", 'CalendarNewsRelease/news_release.py')
+        # self.start_task("calander_news", 'CalendarNewsRelease/news_release.py', 'calendar_news', 'PubDate', '01:00')
+
+        # # 东财财富号 / 每天爬取 1 次
+        # # TODO 暂定 运行完一次时间较长 升频时添加一个制动混合启动
+        # self.docker_run_spider("dongc", 'CArticle/ca_main.py')
+        # self.start_task("dongc", 'CArticle/ca_main.py', "eastmoney_carticle", 'pub_date', "02:00", restart=True)
+
+        # 财新社
+        # 财新社电报 / 每小时爬取一次
+        # 财新社按照新闻编号爬取 / 每小时爬取一次
+        self.docker_run_spider("cls", 'ClsCnInfo/telegraphs.py')
+        self.docker_run_spider("cls2", 'ClsCnInfo/cls_details.py')
+        self.interval_start_task("cls", 'ClsCnInfo/telegraphs.py', 'cls_telegraphs', 'pub_date', (1, "hours"))
+        self.interval_start_task("cls2", 'ClsCnInfo/cls_details.py', 'cls_telegraphs', 'pub_date', (1, "hours"))
+
+        # self.docker_run_spider("gov", 'GovSpiders/gov_main.py')
+        # self.docker_run_spider('juf', 'JfInfo/jfinfo_main.py')
+        # self.docker_run_spider('juc', 'JuchaoInfo/juchao.py')
+        # self.docker_run_spider('m163', 'Money163/netease_money.py')
+        # self.docker_run_spider('qqf', 'QQStock/qq_stock.py')
+        # self.docker_run_spider('shcn', 'ShangHaiSecuritiesNews/cn_main.py')
+        # self.docker_run_spider('sohu', 'sohu/sohu_spider.py')
+        # self.docker_run_spider('stcn', 'StockStcn/kuaixun.py')
+        # self.docker_run_spider('tkg', 'Takungpao/takungpao_main.py')
+        # self.docker_run_spider('tgb', 'Taoguba/tgb_main.py')
+        # self.docker_run_spider('yicai', 'YiCai/yicai_spider.py')
+
+        while True:
+            logger.info("当前调度系统中的任务列表是:\n{}".format(pprint.pformat(schedule.jobs)))
+            schedule.run_pending()
+            time.sleep(10)
 
 
 def main():
-    # docker_run_spider("test_sohu", 'sohu/sohu_spider.py')
-    # docker_run_spider("calander_news", 'CalendarNewsRelease/news_release.py')
-    # docker_run_spider("dongc", 'CArticle/ca_main.py')
-    # docker_run_spider("cls", 'ClsCnInfo/telegraphs.py')
-    # docker_run_spider("cls2", 'ClsCnInfo/cls_details.py')
-    # docker_run_spider("gov", 'GovSpiders/gov_main.py')
-    # docker_run_spider('juf', 'JfInfo/jfinfo_main.py')
-    # docker_run_spider('juc', 'JuchaoInfo/juchao.py')
-    # docker_run_spider('m163', 'Money163/netease_money.py')
-    # docker_run_spider('qqf', 'QQStock/qq_stock.py')
-    # docker_run_spider('shcn', 'ShangHaiSecuritiesNews/cn_main.py')
-    # docker_run_spider('sohu', 'sohu/sohu_spider.py')
-    # docker_run_spider('stcn', 'StockStcn/kuaixun.py')
-    # docker_run_spider('tkg', 'Takungpao/takungpao_main.py')
-    # docker_run_spider('tgb', 'Taoguba/tgb_main.py')
-    docker_run_spider('yicai', 'YiCai/yicai_spider.py')
+    DockerSwith().run()
 
 
 if __name__ == "__main__":
