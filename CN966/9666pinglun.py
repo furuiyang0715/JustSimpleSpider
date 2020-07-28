3114d745c7fcd1f2a90f74aec41b42928171b38e
furuiyang <furuiyang0715@gmail.com>
2020-07-28 11:23:27 +0800

    imp

--- CN966/9666pinglun.py
+++ CN966/9666pinglun.py
@@ -17,6 +17,17 @@ class PingLun9666(SpiderBase):
             'Cookie': 'cowboy_website=2a1644a5-0b40-4d46-93ca-367c54e39220; cowboy_visit_time=1595899251676; Hm_lvt_94656c824ef4eacf1a61cd662f7a7f17=1595899253; __utmc=236883550; __utmz=236883550.1595899253.1.1.utmcsr=cn.bing.com|utmccn=(referral)|utmcmd=referral|utmcct=/; cowboy_login=C3AD5319C3B607C2840F46C286C3AEC2B9C393C2AD3D3060C2896512423AC3B7C3B1596E2B47C282C3A21145C28C3E41C395C393C390C2A7C38B; cowboy_login_imply=C3AD5319C3B607C2840F46C286C3AEC2B9C393C2AD3D3060C2896512423AC3B7C3B1596E2B47C282C3A21145C28C3E41C395C393C390C2A7C38B; cowboy_nick_name=e882a1e58f8b343838393937; cowboy_user_name=nz_d09e0490; cowboy_latest_login_time=1595899414; Hm_lvt_11beb42de0d3859b0af4fd1b6f3b45cf=1595899267,1595899423,1595899565; Hm_lpvt_94656c824ef4eacf1a61cd662f7a7f17=1595902758; Hm_lpvt_11beb42de0d3859b0af4fd1b6f3b45cf=1595902758; __utma=236883550.1639834052.1595899253.1595899253.1595902758.2; __utmt=1; __utmb=236883550.2.9.1595902758; JSESSIONID=DBE042153E24D84C8E659A9DFDB824ED',
         }
 
+    def get_detail(self, url):
+        resp = requests.get(url, headers=self.headers)
+        if resp and resp.status_code == 200:
+            body = resp.text
+            doc = html.fromstring(body)
+            article = doc.xpath("//div[@class='blog-article']")
+            if article:
+                article = article[0].text_content()
+                article = self._process_content(article)
+                return article
+
     def get_list(self, url):
         resp = requests.get(url, headers=self.headers)
         if resp and resp.status_code == 200:
@@ -31,18 +42,21 @@ class PingLun9666(SpiderBase):
                 for box in boxs1:
                     info = box.xpath(".//div[@class='channel_one']/span[@class='f24 fb']/a")[0]
                     link = info.xpath("./@href")[0]
-                    title = info.text_content()
-                    pub_str = box.xpath(".//p[@class='list-info gray01']")[0]
-                    pub_str = pub_str.text_content().strip()
-                    pub_date = re.findall("\d{2}-\d{2} \d{2}:\d{2}", pub_str)[0]
-                    pub_date = str(datetime.datetime.now().year) + "-" + pub_date
-                    print(link)
-                    print(title)
-                    print(pub_date)
-                    print()
+                    article = self.get_detail(link)
+                    if article:
+                        title = info.text_content()
+                        pub_str = box.xpath(".//p[@class='list-info gray01']")[0]
+                        pub_str = pub_str.text_content().strip()
+                        pub_date = re.findall("\d{2}-\d{2} \d{2}:\d{2}", pub_str)[0]
+                        pub_date = str(datetime.datetime.now().year) + "-" + pub_date
+                        print(link)
+                        print(title)
+                        print(pub_date)
+                        print(article)
+                        print()
 
     def start(self):
-        for page_num in range(1, 11):
+        for page_num in range(1, 3):
             url = self.format_url.format(page_num)
             self.get_list(url)
 
