647f8031b6f6965ca6d94f739bf47d4e6bea48f2
furuiyang <furuiyang0715@gmail.com>
2020-07-29 11:10:56 +0800

    尝试使用不同的位移进行破解

--- JustTest/learn_js/demo_1.py
+++ JustTest/learn_js/demo_1.py
@@ -125,4 +125,46 @@ import sys
 #     assert Decrypt(1, Encrypt(1, p)) == p
 
 
+def _move_leter(letter, n):
+    """
+    把字母变为字母表后n位的字母,z后面接a
+    :param letter: 小写字母
+    :param n: 要移动的字母
+    :return: 移动的结果
+    """
+    _first = ord("a")
+    _instance = ord(letter) - _first
+    _new_instance = (_instance + n) % 26
+    _new_char = chr(_new_instance + _first)
+    return _new_char
 
+    # return chr((ord(letter) - ord('a') + n) % 26 + ord('a'))
+
+
+def Decrypt(k, c):
+    """
+    移位密码解密函数D
+    :param k: 秘钥k,每个字母在字母表中移动k位
+    :param c: 密文c
+    :return: 明文p
+    """
+    letter_list = list(c.lower())
+    p = ''.join([_move_leter(x, -k) for x in letter_list])
+    return p
+
+
+def analyze(c):
+    """
+    移位密码分析
+    :param c: 密文c
+    :return:
+    """
+    for k in range(26):
+        # 用不同的秘钥k尝试解密
+        print('秘钥%d：' % k + Decrypt(k, c))
+
+
+if __name__ == '__main__':
+    # 模拟一个破解的过程 尝试不同的位移进行破解
+    c = 'jmpwfdpejoh'
+    analyze(c)
