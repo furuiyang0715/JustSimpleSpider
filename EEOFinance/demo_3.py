2a2ad21628803d9cf7448fee117926d4007e5c31
furuiyang <furuiyang0715@gmail.com>
2020-08-14 15:11:33 +0800

    详情

--- EEOFinance/demo_3.py
+++ EEOFinance/demo_3.py
@@ -1,3 +1,6 @@
+import datetime
+import re
+
 import requests
 from lxml import html
 
@@ -14,5 +17,20 @@ if resp and resp.status_code == 200:
         article = article.text_content()
     except:
         article = ''
-    print(article)
+
+    try:
+        head_part = doc.xpath(".//div[@class='xd-b-b']")[0]
+        pub_info = head_part.xpath("./p")[0]
+        pub_info = pub_info.text_content()
+        pub_date = re.findall(r"\d{4}-\d{2}-\d{2}", pub_info)[0]  # 匹配出时间
+        pub_date = pub_date.strip()
+        pub_date = datetime.datetime.strptime(pub_date, "%Y-%m-%d")
+        author = re.findall(r'[\u4e00-\u9fa5]+', pub_info)[0]  # 匹配出作者
+    except:
+        pub_date = ''
+        author = ''
+
+    print(author, "\n", pub_date, "\n", article)
+
+    # print(article)
 
--- EEOFinance/eeospider.py
+++ EEOFinance/eeospider.py
@@ -1,3 +1,5 @@
+import datetime
+import re
 import sys
 
 import requests
@@ -21,39 +23,74 @@ class EEOSpider(object):
         ]
         self.topic_format_url = 'http://www.eeo.com.cn/{}/'
         self.topic_urls = [self.topic_format_url.format(topic) for topic in self.topic_words]
-        print(self.topic_urls)
+        # print(self.topic_urls)
 
     def get_topic(self, url):
         """获取栏目资讯"""
-        '''
-    <li>
-        <a href="http://www.eeo.com.cn/2020/0813/398971.shtml" target="_blank">
-        <img width=311px height=159px src="http://upload.eeo.com.cn/2020/0813/thumb_311_159_1597304928597.png" />
-            <span class="cls"></span>
-        </a> 
-        <div>
-            <span data-cid="398971"><a href="http://www.eeo.com.cn/2020/0813/398971.shtml" target="_blank">“深圳东·新一代‘智’造中心”华夏顺泽惠阳信息科技园正在出发</a></span>
-            <p>华夏顺泽惠阳信息科技园毗邻深圳东的惠州市惠阳区，正在成为粤港澳大湾区信息科技产业版图上快速崛起的“新贵”。</p>
-            <a href="http://www.eeo.com.cn/eeo/shangyechanye/dashi/">大事</a>							
-        </div>
-    </li>
-        '''
         resp = requests.get(url)
         if resp and resp.status_code == 200:
             text = resp.text.encode("ISO-8859-1").decode("utf-8")
-            # print(text)
             doc = html.fromstring(text)
             articles = doc.xpath(".//div[@class='list']/ul[@id='lyp_article']/li")
+            items = []
             for article in articles:
-                # print(article)
                 link = article.xpath("./a/@href")[0]   # 文章链接
                 title = article.xpath(".//p")[0].text_content()    # 文章标题
-                # print(title)
-                # print(link)
+                ret = self.get_detail(link)
+
+                if not ret:
+                    continue
+
+                elif isinstance(ret, str):
+                    item = {
+                        'link': link,
+                        'title': title,
+                        'article': ret,
+                    }
+
+                else:   # isinstance(ret, tuple):
+                    item = {
+                        'link': link,
+                        'title': title,
+                        'author': ret[0],
+                        'pub_date': ret[1],
+                        'article': ret[2],
+                    }
+                items.append(item)
+            return items
+        return None
+
+    def get_detail(self, url, is_api=False):
+        resp = requests.get(url)
+        if resp and resp.status_code == 200:
+            body = resp.text.encode("ISO-8859-1").decode("utf-8")
+            doc = html.fromstring(body)
+            try:
+                article = doc.xpath(".//div[@class='xx_boxsing']")[0]
+                article = article.text_content()
+            except:
+                article = ''
+
+            if is_api:
+                return article
+            else:
+                try:
+                    head_part = doc.xpath(".//div[@class='xd-b-b']")[0]
+                    pub_info = head_part.xpath("./p")[0]
+                    pub_info = pub_info.text_content()
+                    pub_date = re.findall(r"\d{4}-\d{2}-\d{2}", pub_info)[0]  # 匹配出时间
+                    pub_date = pub_date.strip()
+                    pub_date = datetime.datetime.strptime(pub_date, "%Y-%m-%d")
+                    author = re.findall(r'[\u4e00-\u9fa5]+', pub_info)[0]  # 匹配出作者
+                except:
+                    pub_date = ''
+                    author = ''
+                return author, pub_date, article
+        return None
 
     def start(self):
         for url in self.topic_urls:
-            # print(url)
+            print(url)
             self.get_topic(url)
 
 
